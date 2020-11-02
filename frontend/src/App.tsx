3fd1350b8da8cc175218dc864855ac0e0cbc5448
Emil Wihlander <emil.wihlander@outlook.com>
2020-11-02 10:09:03 +0100

    Implement a new header based on the design from WeKnowIt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,61 +4,43 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import {AppBar, Toolbar, Typography, } from '@material-ui/core';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import './App.css';
+import { Box, createStyles, makeStyles, Theme } from '@material-ui/core';
 
-import GraphQL from './components/GraphQL';
+import GraphQLProvider from './providers/GraphQLProvider';
+import LoginProvider from './providers/LoginProvider';
+import ThemeProvider from './providers/ThemeProvider';
+
 import Body from './components/Body';
-import Login from './components/Login';
-
-import { KeycloakProvider } from '@react-keycloak/web';
-import keycloak from './keycloak';
+import Header from './components/Header';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }),
-);
-
+    base: {
+      backgroundColor: theme.palette.background.default,
+      minHeight: '100%',
+    }
+  })
+)
 
 const App = () => {
   const classes = useStyles();
   return (
-    <KeycloakProvider keycloak={keycloak}>
-      <GraphQL>
-        <BrowserRouter>
-          <div className="App">
-            <AppBar position="static" className={classes.root}>
-              <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                  <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" className={classes.title}>
-                  D-sektionens Medlemssida
-                </Typography>
-                <Login/>
-              </Toolbar>
-            </AppBar>
-            <Switch>
-              <Route exact path="/">
-              </Route>
-            </Switch>
-            <Body/>
-          </div>
-        </BrowserRouter>
-      </GraphQL>
-    </KeycloakProvider>
+    <LoginProvider>
+      <GraphQLProvider>
+        <ThemeProvider>
+          <BrowserRouter>
+            <Box className={classes.base}>
+              <Header/>
+              <Switch>
+                <Route exact path="/">
+                </Route>
+              </Switch>
+              <Body/>
+            </Box>
+          </BrowserRouter>
+        </ThemeProvider>
+      </GraphQLProvider>
+    </LoginProvider>
   );
 }
 
