7b728c4b5594facf65e6b3c37562b4f079843ad0
Emil Wihlander <emil.wihlander@outlook.com>
2020-11-08 16:21:19 +0100

    Add queries and mutations for positions and committees

--- backend/services/members/src/db.ts
+++ backend/services/members/src/db.ts
@@ -1,4 +1,5 @@
-import { database as knex } from 'dsek-shared';
+import { ForbiddenError } from 'apollo-server';
+import { context, knex, dbUtils } from 'dsek-shared';
 
 interface DbMember {
   id: number,
@@ -9,6 +10,19 @@ interface DbMember {
   class_programme: string,
   class_year: number,
   picture_path: string,
+}
+
+interface DbCommittee {
+  id: number,
+  name?: string,
+  name_en?: string,
+}
+
+interface DbPosition {
+  id: number,
+  name?: string,
+  name_en?: string,
+  committee_id?: number,
 }
 
 const getMember = async (identifier: {student_id?: string, id?: number}): Promise<DbMember | undefined> => {
@@ -25,7 +39,96 @@ const getMember = async (identifier: {student_id?: string, id?: number}): Promis
     })
 }
 
+interface PositionFilter {
+  id?: number,
+  name?: string,
+  name_en?: string,
+  committee_id?: number,
+}
+
+const getPositions = (filter?: PositionFilter) => {
+  return knex<DbPosition>('positions').select('*').where(dbUtils.camelToSnake(filter))
+}
+
+interface CreatePosition {
+  name: string,
+  committeeId?: number,
+}
+
+interface UpdatePosition {
+  name?: string,
+  committeeId?: number,
+}
+
+const createPosition = ({user}: context.UserContext, input: CreatePosition) => {
+  if (!user) throw new ForbiddenError('Operation denied');
+  return knex('positions').insert(dbUtils.camelToSnake(input));
+}
+
+const updatePosition = async ({user}: context.UserContext, id: number, input: UpdatePosition) => {
+  if (!user) throw new ForbiddenError('Operation denied');
+  if (Object.keys(input).length === 0) return false;
+  return knex('positions').where({id}).update(dbUtils.camelToSnake(input))
+}
+
+const removePosition = ({user}: context.UserContext, id: number) => {
+  if (!user) throw new ForbiddenError('Operation denied');
+  return knex('positions').where({id}).del()
+}
+
+interface CommitteeFilter {
+  id?: number,
+  name?: string,
+  name_en?: string,
+}
+
+const getCommittees = (filter?: CommitteeFilter) => {
+  return knex<DbCommittee>('committees').select('*').where(dbUtils.camelToSnake(filter))
+}
+
+interface CreateCommittee {
+  name: string,
+}
+
+interface UpdateCommittee {
+  name?: string,
+}
+
+const createCommittee = ({user}: context.UserContext, input: CreateCommittee) => {
+  if (!user) throw new ForbiddenError('Operation denied');
+  return knex('committees').insert(dbUtils.camelToSnake(input))
+}
+
+const updateCommittee = async ({user}: context.UserContext, id: number, input: UpdateCommittee) => {
+  if (!user) throw new ForbiddenError('Operation denied');
+  if (Object.keys(input).length === 0) return false;
+  return knex('committee').where({id}).update(dbUtils.camelToSnake(input))
+}
+
+const removeCommittee = ({user}: context.UserContext, id: number) => {
+  if (!user) throw new ForbiddenError('Operation denied');
+  return knex('committee').where({id}).del()
+}
+
 export {
   DbMember,
   getMember,
+
+  DbPosition,
+  PositionFilter,
+  getPositions,
+  CreatePosition,
+  createPosition,
+  UpdatePosition,
+  updatePosition,
+  removePosition,
+
+  DbCommittee,
+  CommitteeFilter,
+  getCommittees,
+  CreateCommittee,
+  createCommittee,
+  UpdateCommittee,
+  updateCommittee,
+  removeCommittee,
 }
--- backend/services/members/src/resolvers.ts
+++ backend/services/members/src/resolvers.ts
@@ -1,18 +1,83 @@
-import { DbMember, getMember } from './db';
-import { context } from 'dsek-shared';
-
+import {
+  DbMember,
+  getMember,
+  PositionFilter,
+  CommitteeFilter,
+  getCommittees,
+  createCommittee,
+  CreateCommittee,
+  getPositions,
+  DbPosition,
+  UpdateCommittee,
+  updateCommittee,
+  removeCommittee,
+  CreatePosition,
+  createPosition,
+  updatePosition,
+  UpdatePosition,
+  removePosition,
+} from './db';
+import { context, dbUtils } from 'dsek-shared';
 
 export default {
   Query: {
-    async me({}, {}, context: context.UserContext) {
-      if (!context.user?.student_id) return undefined;
-      const me = await getMember({student_id: context.user.student_id});
+    async me({}, {}, {user, roles}: context.UserContext) {
+      if (!user?.student_id) return undefined;
+      const me = await getMember({student_id: user.student_id});
       return me;
+    },
+    positions: ({}, {filter}: {filter?: PositionFilter}) => {
+      return getPositions(filter);
+    },
+    committees: ({}, {filter}: {filter?: CommitteeFilter}) => {
+      return getCommittees(filter);
     }
   },
   Member: {
     async __resolveReference(member: DbMember) {
       return await getMember({id: member.id});
+    }
+  },
+  Committee: {
+    __resolveReference: (committee: CommitteeFilter) =>
+      dbUtils.unique(getCommittees(committee)),
+  },
+  Position: {
+    __resolveReference: (position: PositionFilter) =>
+      dbUtils.unique(getPositions(position)),
+    committee: (parent: DbPosition) =>
+      dbUtils.unique(getCommittees({id: parent.committee_id}))
+  },
+  Mutation: {
+    committee: () => ({}),
+    position: () => ({}),
+  },
+  CommitteeMutations: {
+    create: ({}, {input}: {input: CreateCommittee}, {user, roles}: context.UserContext) => {
+      return createCommittee({user, roles}, input)
+        .then((res) => (res) ? true : false);
+    },
+    update: ({}, {id, input}: {id: number, input: UpdateCommittee}, {user, roles}: context.UserContext) => {
+      return updateCommittee({user, roles}, id, input)
+        .then((res) => (res) ? true : false);
+    },
+    remove: ({}, {id}: {id: number}, {user, roles}: context.UserContext) => {
+      return removeCommittee({user, roles}, id)
+        .then((res) => (res) ? true : false);
+    }
+  },
+  PositionMutations: {
+    create: ({}, {input}: {input: CreatePosition}, {user, roles}: context.UserContext) => {
+      return createPosition({user, roles}, input)
+        .then((res) => (res) ? true : false);
+    },
+    update: ({}, {id, input}: {id: number, input: UpdatePosition}, {user, roles}: context.UserContext) => {
+      return updatePosition({user, roles}, id, input)
+        .then((res) => (res) ? true : false);
+    },
+    remove: ({}, {id}: {id: number}, {user, roles}: context.UserContext) => {
+      return removePosition({user, roles}, id)
+        .then((res) => (res) ? true : false);
     }
   }
 };
