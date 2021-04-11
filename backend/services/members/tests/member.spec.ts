04c9d6913df408a4a3c2f3a1de79af50d19d1df6
Lucas Boberg <lboberg.lb@gmail.com>
2021-04-11 16:36:00 +0200

    feature(#49): Add test for members
    Test for member mutations and queries

--- backend/services/members/tests/member.spec.ts
+++ backend/services/members/tests/member.spec.ts
@@ -5,12 +5,43 @@ import { expect } from 'chai';
 import { context, knex } from 'dsek-shared';
 import MemberAPI from '../src/datasources/Member';
 import { DbMember } from '../src/types/mysql';
+import { CreateMember, UpdateMember } from '../src/types/graphql';
+import { UserInputError } from 'apollo-server-errors';
 
 const members: Partial<DbMember>[] = [
   {id: 1, student_id: 'test'},
   {id: 2, student_id: 'test2'},
   {id: 3, student_id: 'test3'},
 ]
+
+const createMember: CreateMember = {
+  student_id: 'test4',
+  first_name: 'Truls',
+  nickname: 'Trule',
+  last_name: 'Trulsson',
+  class_programme: 'D',
+  class_year: 2203,
+}
+
+const updateMember: UpdateMember = {
+  first_name: 'Trula',
+  nickname: 'Trul',
+  last_name: 'Trulsson',
+  class_programme: 'C',
+  class_year: 2233,
+  picture_path: 'static/image.jpeg'
+}
+
+const updatedMember: DbMember = {
+  id: 1,
+  student_id: 'test',
+  first_name: 'Trula',
+  nickname: 'Trul',
+  last_name: 'Trulsson',
+  class_programme: 'C',
+  class_year: 2233,
+  picture_path: 'static/image.jpeg'
+}
 
 const user: context.UserContext = {
   user: {
@@ -44,6 +75,82 @@ describe('[MemberAPI]', () => {
       })
       const res = await memberAPI.getMember({});
       expect(res).to.deep.equal(undefined)
+    })
+  })
+  describe('[createMember]', () => {
+    it('creates committee', async () => {
+      const id = 1;
+      tracker.on('query', (query) => {
+        expect(query.method).to.equal('insert')
+        Object.values(createMember).forEach(v => expect(query.bindings).to.include(v))
+        query.response([id])
+      })
+      const res = await memberAPI.createMember(createMember)
+      expect(res).to.deep.equal({id, ...createMember})
+    })
+  })
+  describe('[updateMember]', () => {
+    it('throws an error if id is missing', async () => {
+      tracker.on('query', (query, step) => {
+        [
+          () => query.response(1),
+          () => query.response([])
+        ][step - 1]()
+      })
+      try {
+        await memberAPI.updateMember(-1, updateMember)
+        expect.fail('Did not throw Error')
+      } catch (e) {
+        expect(e).to.be.instanceof(UserInputError)
+      }
+    })
+    it('updates member', async () => {
+      const id = 1;
+      tracker.on('query', (query, step) => {
+        [
+          () => {
+            expect(query.method).to.equal('update')
+            Object.values(updateMember).forEach(v => expect(query.bindings).to.include(v))
+            query.response(1)
+          },
+          () => {
+            expect(query.method).to.equal('select')
+            expect(query.bindings).to.include(id)
+            query.response([updatedMember])
+          }
+        ][step - 1]()
+      })
+      const res = await memberAPI.updateMember(id, updateMember)
+      expect(res).to.deep.equal(updatedMember)
+    })
+  })
+  describe('[removeMember]', () => {
+    it('throws an error if id is missing', async () => {
+      tracker.on('query', query => query.response([]));
+      try {
+        await memberAPI.removeMember(-1);
+        expect.fail('did not throw error');
+      } catch(e) {
+        expect(e).to.be.instanceof(UserInputError);
+      }
+    });
+
+    it('removes and returns an member', async () => {
+      const member = members[0];
+      tracker.on('query', (query, step) => {[
+        () => {
+          expect(query.method).to.equal('select');
+          expect(query.bindings).to.include(member.id);
+          query.response([member]);
+        },
+        () => {
+          expect(query.method).to.equal('del');
+          expect(query.bindings).to.include(member.id);
+          query.response(1);
+        },
+      ][step-1]()})
+      const res = await memberAPI.removeMember(<number> member.id);
+      expect(res).to.deep.equal(member);
     })
   })
 })
--- backend/services/members/tests/mutations.spec.ts
+++ backend/services/members/tests/mutations.spec.ts
@@ -6,9 +6,60 @@ import { ApolloServerTestClient, createTestClient } from 'apollo-server-testing'
 
 import { DataSources } from '../src/datasources';
 import { constructTestServer } from './util';
+import { DbMember } from '../src/types/mysql';
+import { Member } from '../src/types/graphql';
 
 chai.use(spies);
 const sandbox = chai.spy.sandbox();
+
+const CREATE_MEMBER = gql`
+mutation createMember {
+  member {
+    create(input: {student_id: "la6547tr-s", first_name: "Gubbe2", last_name: "25", nickname: "Ful", class_programme: "C", class_year: 2200, picture_path: "/static/members/pictures/emil.jpg"}) {
+      id
+      student_id
+      first_name
+      last_name
+      nickname
+      class_year
+      picture_path
+      class_programme
+    }
+  }
+}
+`
+const UPDATE_MEMBER = gql`
+mutation updateMember {
+  member {
+    update(id: 1, input: {first_name: "Gumma4", last_name: "8439"}) {
+      id
+      student_id
+      first_name
+      last_name
+      nickname
+      class_year
+      picture_path
+      class_programme
+    }
+  }
+}
+`
+const REMOVE_MEMBER = gql`
+mutation removeMember {
+  member {
+    remove(id: 1) {
+      id
+      student_id
+      first_name
+      last_name
+      nickname
+      class_year
+      picture_path
+      class_programme
+    }
+  }
+}
+`
 
 const CREATE_POSITION = gql`
 mutation createPosition {
@@ -53,6 +104,17 @@ mutation removeCommittee {
 }
 `
 
+const member: Member = {
+  id: 1,
+  student_id: "la6547tr-s",
+  last_name: "25",
+  first_name: "Gubbe",
+  nickname: "Ful",
+  class_programme: "C",
+  class_year: 2200,
+  picture_path: "/static/members/pictures/emil.jpg",
+}
+
 describe('[Mutations]', () => {
   let server: ApolloServer;
   let dataSources: DataSources;
@@ -68,19 +130,14 @@ describe('[Mutations]', () => {
   })
 
   beforeEach(() => {
-    // Due to an error in @types/chai-spies ts-ignore is necessary.
-    // Pull request solving the issue https://github.com/DefinitelyTyped/DefinitelyTyped/pull/49678§
-    // @ts-ignore
+    sandbox.on(dataSources.memberAPI, 'createMember', (input) => new Promise(resolve => resolve(member)))
+    sandbox.on(dataSources.memberAPI, 'updateMember', (id, input) => new Promise(resolve => resolve(member)))
+    sandbox.on(dataSources.memberAPI, 'removeMember', (id) => new Promise(resolve => resolve(member)))
     sandbox.on(dataSources.positionAPI, 'createPosition', (ctx, input) => new Promise(resolve => resolve(input.name ? true : false)))
-    // @ts-ignore
     sandbox.on(dataSources.positionAPI, 'updatePosition', (ctx, id, input) => new Promise(resolve => resolve(id ? true : false)))
-    // @ts-ignore
     sandbox.on(dataSources.positionAPI, 'removePosition', (ctx, id) => new Promise(resolve => resolve(id ? true : false)))
-    // @ts-ignore
     sandbox.on(dataSources.committeeAPI, 'createCommittee', (ctx, input) => new Promise(resolve => resolve(input.name ? true : false)))
-    // @ts-ignore
     sandbox.on(dataSources.committeeAPI, 'updateCommittee', (ctx, id, input) => new Promise(resolve => resolve(id ? true : false)))
-    // @ts-ignore
     sandbox.on(dataSources.committeeAPI, 'removeCommittee', (ctx, id) => new Promise(resolve => resolve(id ? true : false)))
   })
 
@@ -88,20 +145,62 @@ describe('[Mutations]', () => {
     sandbox.restore();
   })
 
+  describe('[member]', () => {
+
+    it('try creating member no user', async () => {
+      const { errors } = await client.mutate({ mutation: CREATE_MEMBER });
+      expect(errors).to.exist
+    })
+
+    it('creates a member', async () => {
+      const { server, dataSources } = constructTestServer({user: {keycloak_id: 'kc_1'}});
+      const { mutate } = createTestClient(server);
+      sandbox.on(dataSources.memberAPI, 'createMember', (input) => new Promise(resolve => resolve(member)));
+      const { data } = await mutate({ mutation: CREATE_MEMBER });
+      expect(data.member.create).to.deep.equal(member);
+    })
+
+    it('try updating member no user', async () => {
+      const { errors } = await client.mutate({ mutation: UPDATE_MEMBER });
+      expect(errors).to.exist
+    })
+
+    it('updates a member', async () => {
+      const { server, dataSources } = constructTestServer({user: {keycloak_id: 'kc_1'}});
+      const { mutate } = createTestClient(server);
+      sandbox.on(dataSources.memberAPI, 'updateMember', (input) => new Promise(resolve => resolve(member)));
+      const { data } = await mutate({ mutation: UPDATE_MEMBER });
+      expect(data.member.update).to.deep.equal(member);
+    })
+
+    it('try removing member no user', async () => {
+      const { errors } = await client.mutate({ mutation: REMOVE_MEMBER });
+      expect(errors).to.exist
+    })
+
+    it('removes a member', async () => {
+      const { server, dataSources } = constructTestServer({user: {keycloak_id: 'kc_1'}});
+      const { mutate } = createTestClient(server);
+      sandbox.on(dataSources.memberAPI, 'removeMember', (input) => new Promise(resolve => resolve(member)));
+      const { data } = await mutate({ mutation: REMOVE_MEMBER });
+      expect(data.member.remove).to.deep.equal(member);
+    })
+  })
+
   describe('[position]', () => {
 
     it('creates a position', async () => {
-      const { data } = await client.mutate({mutation: CREATE_POSITION});
+      const { data } = await client.mutate({ mutation: CREATE_POSITION });
       expect(data.position.create).to.be.true;
     })
 
     it('updates a position', async () => {
-      const { data } = await client.mutate({mutation: UPDATE_POSITION});
+      const { data } = await client.mutate({ mutation: UPDATE_POSITION });
       expect(data.position.update).to.be.true;
     })
 
     it('removes a position', async () => {
-      const { data } = await client.mutate({mutation: REMOVE_POSITION});
+      const { data } = await client.mutate({ mutation: REMOVE_POSITION });
       expect(data.position.remove).to.be.true;
     })
   })
@@ -109,17 +208,17 @@ describe('[Mutations]', () => {
   describe('[committee]', () => {
 
     it('creates a committee', async () => {
-      const { data } = await client.mutate({mutation: CREATE_COMMITTEE});
+      const { data } = await client.mutate({ mutation: CREATE_COMMITTEE });
       expect(data.committee.create).to.be.true;
     })
 
     it('updates a committee', async () => {
-      const { data } = await client.mutate({mutation: UPDATE_COMMITTEE});
+      const { data } = await client.mutate({ mutation: UPDATE_COMMITTEE });
       expect(data.committee.update).to.be.true;
     })
 
     it('removes a committee', async () => {
-      const { data } = await client.mutate({mutation: REMOVE_COMMITTEE});
+      const { data } = await client.mutate({ mutation: REMOVE_COMMITTEE });
       expect(data.committee.remove).to.be.true;
     })
   })
