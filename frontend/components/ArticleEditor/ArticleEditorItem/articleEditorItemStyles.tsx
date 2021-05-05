c3e72114a9f57185be2deefaf73dfb2f4a628bcf
Noah Mayerhofer <noahmay@users.noreply.github.com>
2021-05-05 02:06:16 +0200

    bugfix(#73): fixes margin on upload button

--- frontend/components/ArticleEditor/ArticleEditorItem/articleEditorItemStyles.tsx
+++ frontend/components/ArticleEditor/ArticleEditorItem/articleEditorItemStyles.tsx
@@ -2,6 +2,11 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const articleEditorItemStyles = makeStyles(theme => ({
     uploadButton: {
-        marginLeft: theme.spacing(2),
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(2),
+        },
+        [theme.breakpoints.down('sm')]: {
+            marginTop: theme.spacing(2),
+        },
     }
 }))
