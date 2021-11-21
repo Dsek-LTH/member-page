0715b37cc3b80fcfc1e64118e6f192f786fbac8d
Noah Mayerhofer <noahmay@users.noreply.github.com>
2021-11-21 17:22:18 +0100

    feature(#182): add door endpoint att /api/door/[doorName] (#250)
    * feature(#182): add door endpoint att /api/door/[doorName]
    
    * fix eslint
    
    * fix comments

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import ArticleSet from '../components/News/articleSet';
 import Grid from '@mui/material/Grid';
 import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
 import { Paper, Link as MuiLink } from '@mui/material';
-import SmallCalendar from '../components/Calendar/SmallCalendar';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import ArticleSet from '../components/News/articleSet';
+import SmallCalendar from '../components/Calendar/SmallCalendar';
 import DefaultLayout from '../layouts/defaultLayout';
 import { useEventsQuery, useGetBookingsQuery } from '~/generated/graphql';
 import routes from '~/routes';
 
-export default function HomePage() {
+const HomePage = function () {
   const { t } = useTranslation('common');
   const {
     data: eventsData,
@@ -24,51 +24,52 @@ export default function HomePage() {
   } = useGetBookingsQuery();
 
   return (
-    <>
-      <DefaultLayout>
-        <Grid
-          container
-          spacing={3}
-          direction="row"
-          justifyContent="center"
-          alignItems="flex-start"
-        >
-          <Grid item xs={12} sm={12} md={7} lg={9}>
-            <Link href="/news">
-              <h2>
-                <MuiLink style={{ color: 'inherit' }} href="/news">
-                  {t('news')}
-                </MuiLink>
-              </h2>
-            </Link>
-            <ArticleSet fullArticles={false} articlesPerPage={10} />
-          </Grid>
-          <Grid item xs={12} sm={12} md={5} lg={3}>
-            <Link href={routes.calendar}>
-              <h2>
-                <MuiLink style={{ color: 'inherit' }} href={routes.calendar}>
-                  {t('calendar')}
-                </MuiLink>
-              </h2>
-            </Link>
-            <Paper>
-              {eventsData &&
-                !eventsLoading &&
-                !bookingsLoading &&
-                !eventsError &&
-                !bookingsError && (
-                  <SmallCalendar
-                    events={eventsData.events}
-                    bookings={bookingsData.bookingRequests}
-                  />
-                )}{' '}
-            </Paper>
-          </Grid>
+    <DefaultLayout>
+      <Grid
+        container
+        spacing={3}
+        direction="row"
+        justifyContent="center"
+        alignItems="flex-start"
+      >
+        <Grid item xs={12} sm={12} md={7} lg={9}>
+          <Link href="/news">
+            <h2>
+              <MuiLink style={{ color: 'inherit' }} href="/news">
+                {t('news')}
+              </MuiLink>
+            </h2>
+          </Link>
+          <ArticleSet fullArticles={false} articlesPerPage={10} />
         </Grid>
-      </DefaultLayout>
-    </>
+        <Grid item xs={12} sm={12} md={5} lg={3}>
+          <Link href={routes.calendar}>
+            <h2>
+              <MuiLink style={{ color: 'inherit' }} href={routes.calendar}>
+                {t('calendar')}
+              </MuiLink>
+            </h2>
+          </Link>
+          <Paper>
+            {eventsData
+              && !eventsLoading
+              && !bookingsLoading
+              && !eventsError
+              && !bookingsError
+              && (
+                <SmallCalendar
+                  events={eventsData.events}
+                  bookings={bookingsData.bookingRequests}
+                />
+              )}
+            {' '}
+          </Paper>
+        </Grid>
+      </Grid>
+    </DefaultLayout>
   );
-}
+};
+export default HomePage;
 
 export const getStaticProps = async ({ locale }) => ({
   props: {
