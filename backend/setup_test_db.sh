f117b952073a406c0be8eeea3b01d22be66c3684
Emil Wihlander <emil.wihlander@outlook.com>
2021-11-21 17:30:14 +0100

    Install dsek-shared

--- backend/setup_test_db.sh
+++ backend/setup_test_db.sh
@@ -1,3 +1,6 @@
 docker run --name postgres-test -p 9999:5432 -e POSTGRES_USER=user -e POSTGRES_PASSWORD=password -e POSTGRES_DB=dsek -d postgres:14-alpine
-cd migrator
+cd shared
+npm install
+npm run build
+cd ../migrator
 NODE_ENV=test node src/migrator.js migrate
