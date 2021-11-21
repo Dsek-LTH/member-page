9253a3843458796f6994cb650831a34f3a0443ab
Emil Wihlander <emil.wihlander@outlook.com>
2021-11-21 22:20:35 +0100

    Add postgres to github actions

--- backend/setup_test_db.sh
+++ backend/setup_test_db.sh
@@ -1,8 +1,5 @@
 docker run --name postgres-test -p 9999:5432 -e POSTGRES_USER=user -e POSTGRES_PASSWORD=password -e POSTGRES_DB=dsek -d postgres:14-alpine
-cd shared
-npm install
-npm run build
-cd ../migrator
+cd migrator
 npm install ../shared
 npm install
 NODE_ENV=test node src/migrator.js migrate
