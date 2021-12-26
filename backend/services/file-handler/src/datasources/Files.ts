6210a44bf9bfa789d036341b31a5a085f8ef060b
Emil Wihlander <emil.wihlander@outlook.com>
2021-12-26 01:44:33 +0100

    refactor(#326): Fromat backend/service/file-handler

--- backend/services/file-handler/src/datasources/Files.ts
+++ backend/services/file-handler/src/datasources/Files.ts
@@ -1,223 +1,210 @@
-import { UserInputError } from "apollo-server";
-import * as gql from '../types/graphql';
+import { UserInputError } from 'apollo-server';
 import path from 'path';
 import { context, dbUtils, minio } from 'dsek-shared';
 import { FileData } from 'chonky';
-import { CopyConditions } from "dsek-shared/dist/minio";
+import * as gql from '../types/graphql';
 
 const minio_base_url = `http://${process.env.MINIO_ENDPOINT || 'http://localhost'}:${process.env.MINIO_PORT || '9000'}/`;
 
+async function fileExists(bucket: string, fileName: string): Promise<boolean> {
+  try {
+    await minio.statObject(bucket, fileName);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
 export default class Files extends dbUtils.KnexDataSource {
+  getFilesInBucket(
+    ctx: context.UserContext,
+    bucket: string,
+    prefix: string,
+  ): Promise<gql.Maybe<gql.FileData[]>> {
+    if (!bucket) {
+      return Promise.resolve([]);
+    }
+    return this.withAccess(`fileHandler:${bucket}:read`, ctx, async () => {
+      const basePath = '';
+      const objectsList = await new Promise<gql.FileData[]>((resolve, reject) => {
+        const stream = minio.listObjectsV2(bucket, prefix !== '/' ? basePath + prefix : basePath, false);
+        const chonkyFiles: gql.FileData[] = [];
 
-
-    getFilesInBucket = (context: context.UserContext, bucket: string, prefix: string): Promise<gql.Maybe<gql.FileData[]>> => {
-        if (!bucket) {
-            return Promise.resolve([]);
-        }
-        return this.withAccess(`fileHandler:${bucket}:read`, context, async () => {
-            const basePath = '';
-            const objectsList = await new Promise<gql.FileData[]>((resolve, reject) => {
-                const stream = minio.listObjectsV2(bucket, prefix !== '/' ? basePath + prefix : basePath, false);
-                const chonkyFiles: gql.FileData[] = [];
-
-                stream.on('data', obj => {
-                    console.log(obj);
-                    if (obj.name) {
-                        chonkyFiles.push({
-                            id: obj.name,
-                            name: path.basename(obj.name),
-                            modDate: obj.lastModified,
-                            size: obj.size,
-                            thumbnailUrl: `${minio_base_url}${bucket}/${obj.name}`
-                        })
-                    }
-                    if (obj.prefix) {
-                        chonkyFiles.push({
-                            id: obj.prefix,
-                            name: path.basename(obj.prefix),
-                            isDir: true
-                        })
-                    }
-                });
-
-                stream.on('error', reject);
-                stream.on('end', () => {
-                    resolve(chonkyFiles);
-                });
+        stream.on('data', (obj) => {
+          if (obj.name) {
+            chonkyFiles.push({
+              id: obj.name,
+              name: path.basename(obj.name),
+              modDate: obj.lastModified,
+              size: obj.size,
+              thumbnailUrl: `${minio_base_url}${bucket}/${obj.name}`,
             });
-            return objectsList;
-        });
-    }
-
-    getPresignedPutUrl = (context: context.UserContext, bucket: string, fileName: string): Promise<gql.Maybe<string>> =>
-        this.withAccess(`fileHandler:${bucket}:create`, context, async () => {
-            if (fileName == '') return undefined;
-
-            const hour = 60 * 60;
-            if (await this.fileExists(bucket, fileName)) {
-                throw new UserInputError(`File ${fileName} already exists`);
-            }
-            let url: string = await minio.presignedPutObject(bucket, fileName, hour)
-            return url;
+          }
+          if (obj.prefix) {
+            chonkyFiles.push({
+              id: obj.prefix,
+              name: path.basename(obj.prefix),
+              isDir: true,
+            });
+          }
         });
 
-    removeObjects = (context: context.UserContext, bucket: string, fileNames: string[]) =>
-        this.withAccess(`fileHandler:${bucket}:delete`, context, async () => {
-            const deleted: FileData[] = [];
-
-            await Promise.all(fileNames.map(async (fileName) => {
-                if (fileName.charAt(fileName.length - 1) === '/') {
-                    const filesInFolder = await this.getFilesInBucket(context, bucket, fileName);
-                    if (filesInFolder) {
-                        this.removeObjects(context, bucket, filesInFolder.map(file => file.id));
-                    }
-                    deleted.push({
-                        id: fileName,
-                        name: path.basename(fileName)
-                    });
-                }
-                else {
-                    await minio.removeObject(bucket, fileName);
-                    deleted.push({
-                        id: fileName,
-                        name: path.basename(fileName)
-                    });
-                }
-            }));
-            return deleted;
+        stream.on('error', reject);
+        stream.on('end', () => {
+          resolve(chonkyFiles);
         });
+      });
+      return objectsList;
+    });
+  }
 
-    moveObject = (context: context.UserContext, bucket: string, fileNames: string[], newFolder: string) =>
-        this.withAccess(`fileHandler:${bucket}:update`, context, async () => {
-            const conditions = new CopyConditions();
-            const moved: gql.FileChange[] = [];
+  getPresignedPutUrl(
+    ctx: context.UserContext,
+    bucket: string,
+    fileName: string,
+  ): Promise<gql.Maybe<string>> {
+    return this.withAccess(`fileHandler:${bucket}:create`, ctx, async () => {
+      if (fileName === '') return undefined;
 
-            await Promise.all(fileNames.map(async (fileName) => {
+      const hour = 60 * 60;
+      if (await fileExists(bucket, fileName)) {
+        throw new UserInputError(`File ${fileName} already exists`);
+      }
+      const url: string = await minio.presignedPutObject(bucket, fileName, hour);
+      return url;
+    });
+  }
 
-                const basename = path.basename(fileName);
+  removeObjects(ctx: context.UserContext, bucket: string, fileNames: string[]) {
+    return this.withAccess(`fileHandler:${bucket}:delete`, ctx, async () => {
+      const deleted: FileData[] = [];
 
-                if (fileName.charAt(fileName.length - 1) === '/') {
-                    const filesInFolder = await this.getFilesInBucket(context, bucket, fileName);
-                    if (filesInFolder) {
-                        const recursivedMoved = await this.moveObject(context, bucket, filesInFolder.map(file => file.id), newFolder + basename + '/');
-                        const FileChange = {
-                            file: { id: newFolder + basename + '/', name: basename, isDir: true },
-                            oldFile: { id: fileName, name: basename, isDir: true },
-                        }
-                        moved.push(FileChange)
-                        moved.push(...recursivedMoved);
-                    }
-                }
-                else {
-                    const newFileName = path.join(newFolder, basename);
-                    let objectStream = undefined
-                    let objectStats = undefined
-
-                    try {
-                        objectStream = await minio.getObject(bucket, fileName);
-                        objectStats = await minio.statObject(bucket, fileName);
-                    } catch (error) {
-                        console.log(error);
-                        return;
-                    }
-
-                    if (await this.fileExists(bucket, newFileName)) {
-                        return;
-                    }
-
-                    const oldFile = {
-                        id: fileName,
-                        name: path.basename(fileName),
-                        modDate: objectStats.lastModified,
-                        size: objectStats.size,
-                        thumbnailUrl: `${minio_base_url}${bucket}/${fileName}`
-                    }
-
-                    const newFile = {
-                        id: newFileName,
-                        name: path.basename(newFileName),
-                        size: objectStats.size,
-                        thumbnailUrl: `${minio_base_url}${bucket}/${newFileName}`
-                    }
-
-                    await minio.putObject(bucket, newFileName, (await objectStream), (await objectStats).size)
-
-                    await minio.removeObject(bucket, fileName);
-
-                    const FileChange = {
-                        file: newFile,
-                        oldFile: oldFile,
-                    }
-
-                    moved.push(FileChange);
-                }
-
-            }));
-            return moved;
-        });
-
-    renameObject = (context: context.UserContext, bucket: string, fileName: string, newFileName: string) =>
-        this.withAccess(`fileHandler:${bucket}:update`, context, async () => {
-
-            const dirname = path.dirname(fileName);
-
-            if (fileName.charAt(fileName.length - 1) === '/') {
-                const filesInFolder = await this.getFilesInBucket(context, bucket, fileName);
-                if (filesInFolder) {
-                    this.moveObject(context, bucket, filesInFolder.map(file => file.id), dirname + newFileName + '/');
-                }
-            }
-            else {
-                const newFileId = path.join(dirname + '/', newFileName);
-                let objectStream = undefined
-                let objectStats = undefined
-
-                try {
-                    objectStream = await minio.getObject(bucket, fileName);
-                    objectStats = await minio.statObject(bucket, fileName);
-                } catch (error) {
-                    console.log(error);
-                    return;
-                }
-
-                if (await this.fileExists(bucket, newFileId)) {
-                    return;
-                }
-
-                const oldFile = {
-                    id: fileName,
-                    name: path.basename(fileName),
-                    modDate: objectStats.lastModified,
-                    size: objectStats.size,
-                    thumbnailUrl: `${minio_base_url}${bucket}/${fileName}`
-                }
-
-                const newFile = {
-                    id: newFileId,
-                    name: path.basename(newFileId),
-                    size: objectStats.size,
-                    thumbnailUrl: `${minio_base_url}${bucket}/${newFileId}`
-                }
-
-                await minio.putObject(bucket, newFileName, (await objectStream), (await objectStats).size)
-
-                await minio.removeObject(bucket, fileName);
-
-                const FileChange = {
-                    file: newFile,
-                    oldFile: oldFile,
-                }
-
-                return (FileChange);
-            }
-        });
-
-    private async fileExists(bucket: string, fileName: string): Promise<boolean> {
-        try {
-            await minio.statObject(bucket, fileName);
-            return true;
-        } catch (error) {
-            return false;
+      await Promise.all(fileNames.map(async (fileName) => {
+        if (fileName.charAt(fileName.length - 1) === '/') {
+          const filesInFolder = await this.getFilesInBucket(ctx, bucket, fileName);
+          if (filesInFolder) {
+            this.removeObjects(ctx, bucket, filesInFolder.map((file) => file.id));
+          }
+          deleted.push({
+            id: fileName,
+            name: path.basename(fileName),
+          });
+        } else {
+          await minio.removeObject(bucket, fileName);
+          deleted.push({
+            id: fileName,
+            name: path.basename(fileName),
+          });
         }
-    }
+      }));
+      return deleted;
+    });
+  }
+
+  moveObject(ctx: context.UserContext, bucket: string, fileNames: string[], newFolder: string) {
+    return this.withAccess(`fileHandler:${bucket}:update`, ctx, async () => {
+      const moved: gql.FileChange[] = [];
+
+      await Promise.all(fileNames.map(async (fileName) => {
+        const basename = path.basename(fileName);
+
+        if (fileName.charAt(fileName.length - 1) === '/') {
+          const filesInFolder = await this.getFilesInBucket(ctx, bucket, fileName);
+          if (filesInFolder) {
+            const recursivedMoved = await this.moveObject(ctx, bucket, filesInFolder.map((file) => file.id), `${newFolder + basename}/`);
+            const FileChange = {
+              file: { id: `${newFolder + basename}/`, name: basename, isDir: true },
+              oldFile: { id: fileName, name: basename, isDir: true },
+            };
+            moved.push(FileChange);
+            moved.push(...recursivedMoved);
+          }
+        } else {
+          const newFileName = path.join(newFolder, basename);
+
+          const objectStream = await minio.getObject(bucket, fileName);
+          const objectStats = await minio.statObject(bucket, fileName);
+
+          if (await fileExists(bucket, newFileName)) {
+            return;
+          }
+
+          const oldFile = {
+            id: fileName,
+            name: path.basename(fileName),
+            modDate: objectStats.lastModified,
+            size: objectStats.size,
+            thumbnailUrl: `${minio_base_url}${bucket}/${fileName}`,
+          };
+
+          const newFile = {
+            id: newFileName,
+            name: path.basename(newFileName),
+            size: objectStats.size,
+            thumbnailUrl: `${minio_base_url}${bucket}/${newFileName}`,
+          };
+
+          await minio.putObject(bucket, newFileName, objectStream, objectStats.size);
+
+          await minio.removeObject(bucket, fileName);
+
+          const FileChange = {
+            file: newFile,
+            oldFile,
+          };
+
+          moved.push(FileChange);
+        }
+      }));
+      return moved;
+    });
+  }
+
+  renameObject(ctx: context.UserContext, bucket: string, fileName: string, newFileName: string) {
+    return this.withAccess(`fileHandler:${bucket}:update`, ctx, async () => {
+      const dirname = path.dirname(fileName);
+
+      if (fileName.charAt(fileName.length - 1) === '/') {
+        const filesInFolder = await this.getFilesInBucket(ctx, bucket, fileName);
+        if (filesInFolder) {
+          this.moveObject(ctx, bucket, filesInFolder.map((file) => file.id), `${dirname + newFileName}/`);
+        }
+        return undefined;
+      }
+      const newFileId = path.join(`${dirname}/`, newFileName);
+
+      const objectStream = await minio.getObject(bucket, fileName);
+      const objectStats = await minio.statObject(bucket, fileName);
+
+      if (await fileExists(bucket, newFileId)) {
+        return undefined;
+      }
+
+      const oldFile = {
+        id: fileName,
+        name: path.basename(fileName),
+        modDate: objectStats.lastModified,
+        size: objectStats.size,
+        thumbnailUrl: `${minio_base_url}${bucket}/${fileName}`,
+      };
+
+      const newFile = {
+        id: newFileId,
+        name: path.basename(newFileId),
+        size: objectStats.size,
+        thumbnailUrl: `${minio_base_url}${bucket}/${newFileId}`,
+      };
+
+      await minio.putObject(bucket, newFileName, (await objectStream), (await objectStats).size);
+
+      await minio.removeObject(bucket, fileName);
+
+      const FileChange = {
+        file: newFile,
+        oldFile,
+      };
+
+      return FileChange;
+    });
+  }
 }
--- backend/services/file-handler/src/resolvers.ts
+++ backend/services/file-handler/src/resolvers.ts
@@ -1,4 +1,3 @@
-import { AuthenticationError } from 'apollo-server';
 import { context } from 'dsek-shared';
 import { DataSources } from './datasources';
 import { Resolvers } from './types/graphql';
@@ -9,27 +8,27 @@ interface DataSourceContext {
 
 const resolvers: Resolvers<context.UserContext & DataSourceContext> = {
   Query: {
-    files: (_, { bucket, prefix }, { user, roles, dataSources }) => {
-      return dataSources.filesAPI.getFilesInBucket({user, roles}, bucket, prefix);
+    files(_, { bucket, prefix }, { user, roles, dataSources }) {
+      return dataSources.filesAPI.getFilesInBucket({ user, roles }, bucket, prefix);
     },
-    presignedPutUrl: (_, { bucket, fileName }, { user, roles, dataSources }) => {
-      return dataSources.filesAPI.getPresignedPutUrl({user, roles}, bucket, fileName);
+    presignedPutUrl(_, { bucket, fileName }, { user, roles, dataSources }) {
+      return dataSources.filesAPI.getPresignedPutUrl({ user, roles }, bucket, fileName);
     },
   },
   Mutation: {
     files: () => ({}),
   },
   FileMutations: {
-    remove: async (_, { bucket, fileNames }, { user, roles, dataSources }) => {
-      return await dataSources.filesAPI.removeObjects({user, roles}, bucket, fileNames);
+    remove(_, { bucket, fileNames }, { user, roles, dataSources }) {
+      return dataSources.filesAPI.removeObjects({ user, roles }, bucket, fileNames);
     },
-    move: (_, { bucket, fileNames, newFolder }, { user, roles, dataSources }) => {
-      return dataSources.filesAPI.moveObject({user, roles}, bucket, fileNames, newFolder);
+    move(_, { bucket, fileNames, newFolder }, { user, roles, dataSources }) {
+      return dataSources.filesAPI.moveObject({ user, roles }, bucket, fileNames, newFolder);
     },
-    rename: (_, { bucket, fileName, newFileName }, { user, roles, dataSources }) => {
-      return dataSources.filesAPI.renameObject({user, roles}, bucket, fileName, newFileName);
+    rename(_, { bucket, fileName, newFileName }, { user, roles, dataSources }) {
+      return dataSources.filesAPI.renameObject({ user, roles }, bucket, fileName, newFileName);
     },
-  }
+  },
 };
 
 export default resolvers;
