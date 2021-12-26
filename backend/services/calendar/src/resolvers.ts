d625098150493ee9c763e5c75c5cc62bf874bf2e
Emil Wihlander <emil.wihlander@outlook.com>
2021-12-26 01:12:15 +0100

    refactor(#326): Fromat backend/service/calendar

--- backend/services/calendar/src/resolvers.ts
+++ backend/services/calendar/src/resolvers.ts
@@ -1,6 +1,6 @@
-import { context } from "dsek-shared";
-import { DataSources } from "./datasources";
-import { Resolvers } from "./types/graphql";
+import { context } from 'dsek-shared';
+import { DataSources } from './datasources';
+import { Resolvers } from './types/graphql';
 
 interface DataSourceContext {
   dataSources: DataSources;
@@ -8,24 +8,24 @@ interface DataSourceContext {
 
 const resolvers: Resolvers<context.UserContext & DataSourceContext> = {
   Query: {
-    event: (_, { id }, { user, roles, dataSources }) => {
+    event(_, { id }, { user, roles, dataSources }) {
       return dataSources.eventAPI.getEvent({ user, roles }, id);
     },
-    events: (_, { page, perPage, filter }, { user, roles, dataSources }) => {
+    events(_, { page, perPage, filter }, { user, roles, dataSources }) {
       return dataSources.eventAPI.getEvents({ user, roles }, page, perPage, filter);
-    }
+    },
   },
   Mutation: {
     event: () => ({}),
   },
   EventMutations: {
-    create: (_, { input }, { user, roles, dataSources }) => {
+    create(_, { input }, { user, roles, dataSources }) {
       return dataSources.eventAPI.createEvent({ user, roles }, input);
     },
-    update: (_, { id, input }, { user, roles, dataSources }) => {
+    update(_, { id, input }, { user, roles, dataSources }) {
       return dataSources.eventAPI.updateEvent({ user, roles }, id, input);
     },
-    remove: (_, { id }, { user, roles, dataSources }) => {
+    remove(_, { id }, { user, roles, dataSources }) {
       return dataSources.eventAPI.removeEvent({ user, roles }, id);
     },
   },
