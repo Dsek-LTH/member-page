89b466a39e3e367a649102cbbfa123523de8b9a0
Lucas Boberg <lucaspboberg@gmail.com>
2021-12-30 13:58:34 +0100

    fixes comments

--- frontend/pages/api/search.ts
+++ frontend/pages/api/search.ts
@@ -2,6 +2,11 @@ import MeiliSearch from 'meilisearch';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { MemberHit } from '~/types/MemberHit';
 
+const client = new MeiliSearch({
+  host: process.env.MEILI_HOST,
+  apiKey: process.env.MEILI_MASTER_KEY,
+});
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
     res.status(404);
@@ -9,11 +14,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const searchQuery = req.query.q as string;
   const escapedQuery = searchQuery.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
-
-  const client = new MeiliSearch({
-    host: process.env.MEILI_HOST,
-    apiKey: process.env.MEILI_MASTER_KEY,
-  });
 
   const result = await client.index('members').search<MemberHit>(escapedQuery);
 
