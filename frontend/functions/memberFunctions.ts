ee21b6a9614587ba75002964fd06283634caa352
oliverlevay <oliver.levay@hotmail.com>
2022-01-30 00:08:30 +0100

    Fix more build errors

--- frontend/functions/memberFunctions.ts
+++ frontend/functions/memberFunctions.ts
@@ -1,6 +1,6 @@
-import { Member, MemberPageQueryResult } from '~/generated/graphql';
+import { MeHeaderQuery, Member, MemberPageQueryResult } from '~/generated/graphql';
 
-export const getFullName = (member: Member | MemberPageQueryResult['data']['memberById'], useNickname = true) =>
+export const getFullName = (member: Member | MeHeaderQuery['me'] | MemberPageQueryResult['data']['memberById'], useNickname = true) =>
   `${member.first_name} ${member.nickname && useNickname ? `"${member.nickname}"` : ''} ${member.last_name}`;
 
 export const getClassYear = (member: Member | MemberPageQueryResult['data']['memberById']) => `${member.class_programme}${member.class_year % 100}`;
