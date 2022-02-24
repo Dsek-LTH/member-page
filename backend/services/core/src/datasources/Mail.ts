c54a72b7a1f85ec3fc1985606014523488bf0cad
Noah Mayerhofer <noahmay@users.noreply.github.com>
2022-02-24 21:14:41 +0100

    add userHasAccessToAlias

--- backend/services/core/src/datasources/Mail.ts
+++ backend/services/core/src/datasources/Mail.ts
@@ -3,13 +3,14 @@ import * as gql from '../types/graphql';
 import * as sql from '../types/database';
 import kcClient from '../keycloak';
 import type { DataSources } from '../datasources';
+import { todayInInterval } from './Mandate';
 
 export default class MailAPI extends dbUtils.KnexDataSource {
-  resolveAlias(
+  getMandatesFromAlias(
     ctx: context.UserContext,
     datasources: DataSources,
     alias: string,
-  ): Promise<gql.Maybe<string[]>> {
+  ): Promise<Array<gql.Maybe<gql.Mandate>>> {
     return this.withAccess('core:mail:alias:read', ctx, async () => {
       const postion_row = await this.knex<sql.Position>('mail_aliases')
         .select('position_id')
@@ -17,32 +18,60 @@ export default class MailAPI extends dbUtils.KnexDataSource {
       const position_ids = postion_row.map((row) => row.position_id);
 
       let page = 0;
-
       let mandates: Array<gql.Maybe<gql.Mandate>> = [];
       let mandatePage;
-
       while (page === 0 || mandatePage?.pageInfo?.hasNextPage) {
         // eslint-disable-next-line no-await-in-loop
         mandatePage = await datasources.mandateAPI.getMandates(ctx, page, 10, {
           position_ids,
         });
+
         mandates = mandates.concat(mandatePage.mandates);
         page += 1;
+      }
+
+      mandates = mandates.filter((m) =>
+        todayInInterval(new Date(m?.start_date), new Date(m?.end_date)));
+      return mandates;
+    });
+  }
+
+  resolveAlias(
+    ctx: context.UserContext,
+    datasources: DataSources,
+    alias: string,
+  ): Promise<gql.Maybe<string[]>> {
+    return this.withAccess('core:mail:alias:read', ctx, async () => {
+      const mandates = await this.getMandatesFromAlias(ctx, datasources, alias);
+
+      if (mandates.length === 0) {
+        return ['root@dsek.se'];
       }
 
       const members = mandates
         .map((mandate) => mandate?.member)
         .map((member) => (member?.id ? member.id : ''));
-      /* console.log("help");
-             if (!members) {
-                 return ["root@dsek.se"]
-             } */
 
       const keycloak_ids = (
         await datasources.memberAPI.getKeycloakIdsFromMemberIds(members)
       )?.map((keycloakRow) => keycloakRow.keycloak_id);
 
       return kcClient.getUserEmails(keycloak_ids ?? []);
+    });
+  }
+
+  userHasAccessToAlias(
+    ctx: context.UserContext,
+    datasources: DataSources,
+    alias: string,
+    studentId: string,
+  ): Promise<boolean> {
+    return this.withAccess('core:mail:alias:read', ctx, async () => {
+      const mandates = await this.getMandatesFromAlias(ctx, datasources, alias);
+      const foundMandate = mandates.find(
+        (mandate) => mandate?.member?.student_id === studentId,
+      );
+      return !!foundMandate;
     });
   }
 }
