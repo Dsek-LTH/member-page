c9efa06a5346a686d6c4f36d3eddc42b3da95210
jacobast <astrand.jacob@gmail.com>
2022-02-28 13:17:54 +0100

    Fixed Lint issues

--- frontend/components/Positions/index.tsx
+++ frontend/components/Positions/index.tsx
@@ -1,12 +1,12 @@
 import { Stack, Typography } from '@mui/material';
 import { styled } from '@mui/system';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import usePositions from '~/hooks/usePositions';
 import Link from '~/components/Link';
 import Position from './Position';
 import routes from '~/routes';
-import { useTranslation } from 'react-i18next';
 
 const PositionsContainer = styled(Stack)`
   display: flex;
@@ -17,7 +17,7 @@ const PositionsContainer = styled(Stack)`
 
 function Positions({ committeeId }: { committeeId: string }) {
   const { positions, loading } = usePositions(committeeId);
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   return (
     <Stack spacing={2}>
       <Stack direction="row" alignItems="center" spacing={2}>
@@ -32,11 +32,11 @@ function Positions({ committeeId }: { committeeId: string }) {
         >
           {positions.length > 0
             ? positions[0].committee.name
-            : t("committee:noPositions")}
+            : t('committee:noPositions')}
         </Typography>
       </Stack>
       <Typography margin="1rem 0 !important">
-        {t("committee:wipDescription")}
+        {t('committee:wipDescription')}
       </Typography>
       <PositionsContainer>
         {positions.map((position) => (
