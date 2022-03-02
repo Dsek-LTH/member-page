723f8c6796e6172bf27a8352b1eebdd546ec8479
Noah Mayerhofer <noahmay@users.noreply.github.com>
2022-03-02 12:17:08 +0100

    feature(#382): Add endpoint to convert mail alias to emails (#385)
    * Update seed to have more dwww members currently
    
    * prevent keycloak mandate updates if NODE_ENV is not production
    
    * feature(#382): add alias resolve api
    
    * refactor: pagnation
    
    * fix lint errors
    
    * add userHasAccessToAlias
    
    * remove console.log
    
    * add resolver for userhasaccesstoalias
    
    * Fix member resolution
    
    * add api endpoint for mail/studentid
    
    * fix testst
    
    Co-authored-by: Oliver Levay <oliver.levay@hotmail.com>

---
+++ backend/migrator/migrations/20220224154000_.mail-aliases-table.js
@@ -0,0 +1,13 @@
+exports.up = (knex) =>
+  knex.schema
+    .createTable('mail_aliases', (t) => {
+      t.comment('Connecting positions with all mail aliases');
+      t.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+      t.string('position_id').comment('A unique id assigned to every position e.g. dsek.srd.mastare');
+      t.foreign('position_id').references('positions.id');
+      t.string('email_alias');
+    });
+
+exports.down = (knex) =>
+  knex.schema
+    .dropTable('mail_aliases');
