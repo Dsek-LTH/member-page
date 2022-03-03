edd2173815557c1208f76d5ca0d993f08ead7eea
Oliver Levay <oliver.levay@hotmail.com>
2022-03-03 14:54:59 +0100

    Make sure images aren't too wide

--- frontend/components/ArticleEditor/ArticleEditorItem/index.tsx
+++ frontend/components/ArticleEditor/ArticleEditorItem/index.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'next-i18next';
 import ReactMde from 'react-mde';
 import 'react-mde/lib/styles/css/react-mde-all.css';
 import ReactMarkdown from 'react-markdown';
+import '~/styles/markdown.module.css';
 import {
   Button,
   FormControl,
