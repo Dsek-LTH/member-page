66d0ad8e0d940deebebf75d2fa3da4f1c9a4683e
Oliver Levay <oliver.levay@hotmail.com>
2022-05-05 20:07:59 +0200

    Add new scheduler and add in_keycloak flag

--- backend/services/core/src/index.ts
+++ backend/services/core/src/index.ts
@@ -13,24 +13,32 @@ schedule('0 0 * * *', async () => {
   logger.info('Updating keycloak mandates');
 
   const today = new Date();
-  const yesterday = new Date(today.getTime() - 24 * 60 * 60 * 1000);
+  const yesterday = new Date(today.getTime() - 24 * 60 * 60 * 1000).toISOString().substring(0, 10);
 
-  const newMandates = await knex<{ keycloak_id: string, position_id: string }>('mandates').join('keycloak', 'mandates.member_id', 'keycloak.member_id').where({ start_date: today.toISOString().substring(0, 10) }).select('keycloak_id', 'position_id');
-  logger.info(`Found ${newMandates.length} new mandates`);
+  const expiredMandates = await knex('mandates').join('keycloak', 'mandates.member_id', 'mandates.id', 'keycloak.member_id').where('end_date', '<', yesterday).where({ in_keycloak: true })
+    .select('keycloak_id', 'position_id');
+  logger.info(`Found ${expiredMandates.length} expired mandates.`);
 
-  const oldMandates = await knex('mandates').join('keycloak', 'mandates.member_id', 'keycloak.member_id').where({ end_date: yesterday.toISOString().substring(0, 10) }).select('keycloak_id', 'position_id');
-  logger.info(`Found ${oldMandates.length} old mandates`);
+  const mandatesToAdd = await knex<{ keycloak_id: string, position_id: string }>('mandates').join('keycloak', 'mandates.member_id', 'mandates.id', 'keycloak.member_id').where('start_date', '>', yesterday).where({ in_keycloak: false })
+    .select('keycloak_id', 'position_id');
+  logger.info(`Found ${mandatesToAdd.length} mandates to add.`);
 
   logger.info('Updating keycloak...');
-  await Promise.all(newMandates.map((mandate) => kcClient
+  await Promise.all(mandatesToAdd.map((mandate) => kcClient
     .createMandate(mandate.keycloak_id, mandate.position_id)
-    .then(() => logger.info(`Added mandate ${mandate.keycloak_id}->${mandate.position_id}`))
-    .catch(() => logger.info(`Failed to add mandate ${mandate.keycloak_id}->${mandate.position_id}`))));
+    .then(async () => {
+      await knex('mandates').where({ id: mandate.keycloak_id }).update({ in_keycloak: true });
+      logger.info(`Created mandate ${mandate.keycloak_id}->${mandate.position_id}`);
+    })
+    .catch(() => logger.info(`Failed to create mandate ${mandate.keycloak_id}->${mandate.position_id}`))));
 
-  await Promise.all(oldMandates.map((mandate) => kcClient
+  await Promise.all(expiredMandates.map((mandate) => kcClient
     .deleteMandate(mandate.keycloak_id, mandate.position_id)
-    .then(() => logger.info(`Added mandate ${mandate.keycloak_id}->${mandate.position_id}`))
-    .catch(() => logger.info(`Failed to add mandate ${mandate.keycloak_id}->${mandate.position_id}`))));
+    .then(async () => {
+      await knex('mandates').where({ id: mandate.keycloak_id }).update({ in_keycloak: false });
+      logger.info(`Deleted mandate ${mandate.keycloak_id}->${mandate.position_id}`);
+    })
+    .catch(() => logger.info(`Failed to delete mandate ${mandate.keycloak_id}->${mandate.position_id}`))));
   logger.info('Done updating mandates');
 });
 
