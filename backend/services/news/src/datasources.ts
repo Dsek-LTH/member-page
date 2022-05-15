cf0f8afd5e66ea55b0c4650b63b3d3cb4bb0e0ff
Oliver Levay <oliver.levay@hotmail.com>
2022-05-15 21:01:28 +0200

    Notifications (#421)
    * add expo_tokens table
    
    * Add stuff
    
    * Fix tests
    
    * Add data to notifications
    
    * Fix duplicated seed

--- backend/services/news/src/datasources.ts
+++ backend/services/news/src/datasources.ts
@@ -1,14 +1,16 @@
 import { knex } from 'dsek-shared';
 import MarkdownsAPI from './datasources/Markdowns';
-
 import NewsAPI from './datasources/News';
+import NotificationsAPI from './datasources/Notifications';
 
 export interface DataSources {
   newsAPI: NewsAPI,
   markdownsAPI: MarkdownsAPI,
+  notifications: NotificationsAPI,
 }
 
 export default () => ({
   newsAPI: new NewsAPI(knex),
   markdownsAPI: new MarkdownsAPI(knex),
+  notifications: new NotificationsAPI(knex),
 });
