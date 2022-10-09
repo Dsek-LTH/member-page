7a714afd64960032fa4824c17937ff7c845bf1a4
Oliver Levay <oliver.levay@hotmail.com>
2022-10-09 23:09:01 +0200

    make it possible to create empty folders

--- frontend/components/Documents/proccessFilesData.ts
+++ frontend/components/Documents/proccessFilesData.ts
@@ -12,7 +12,7 @@ export type Document = {
 
 export default function proccessFilesData(year: string, files: FilesQuery['files']): Meeting[] {
   const meetings = [];
-  const meetingNames = new Set(files.filter((file) => file.id.includes(`/${year}/`)).map((file) => file.id.split('/')[2]).reverse());
+  const meetingNames = new Set(files.filter((file) => file.id.includes(`/${year}/`) && !file.id.includes('_folder-preserver')).map((file) => file.id.split('/')[2]).reverse());
   meetingNames.forEach((meeting) => {
     meetings.push({ title: meeting, files: files.filter((file) => file.id.includes(`/${year}/${meeting}`) && !file.id.includes('blob.png')).map((file) => ({ title: file.name, ...file })) });
   });
