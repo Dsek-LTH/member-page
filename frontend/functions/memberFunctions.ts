94b57c622afaa3433d9826bc36f0751dd9de6334
fnurkla <fnurkla@fnurkla.se>
2022-10-16 15:49:01 +0200

    Fix double spacing in members full names with no nickname (#494)

--- frontend/functions/memberFunctions.ts
+++ frontend/functions/memberFunctions.ts
@@ -1,6 +1,6 @@
 import { MeHeaderQuery, Member, MemberPageQueryResult } from '~/generated/graphql';
 
 export const getFullName = (member: Member | MeHeaderQuery['me'] | MemberPageQueryResult['data']['memberById'], useNickname = true) =>
-  `${member.first_name} ${member.nickname && useNickname ? `"${member.nickname}"` : ''} ${member.last_name}`;
+  `${member.first_name} ${member.nickname && useNickname ? `"${member.nickname}" ` : ''}${member.last_name}`;
 
 export const getClassYear = (member: Member | MemberPageQueryResult['data']['memberById']) => `${member.class_programme}${member.class_year % 100}`;
