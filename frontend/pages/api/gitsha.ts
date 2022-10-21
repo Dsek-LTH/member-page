9eea8c76f6e816d5487e5cbd9f7a0b86e548dc50
Oliver Levay <oliver.levay@hotmail.com>
2022-10-21 10:18:34 +0200

    Fix bugs

--- frontend/pages/api/gitsha.ts
+++ frontend/pages/api/gitsha.ts
@@ -1,8 +1,13 @@
-import { execSync } from 'child_process';
+// import { execSync } from 'child_process';
+import { readFileSync } from 'fs';
 import { SHAResult } from '~/types/SHAResult';
 
 export default function handler(req, res) {
-  const sha = execSync('git rev-parse HEAD').toString().trim();
+  let sha = readFileSync('.git/HEAD').toString().trim();
+  if (sha.indexOf(':') !== -1) {
+    sha = readFileSync(`.git/${sha.substring(5)}`).toString().trim();
+  }
+
   const result: SHAResult = {
     fullSHA: sha,
     shortSHA: sha.substring(0, 7),
