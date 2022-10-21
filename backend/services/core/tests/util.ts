07de5414fc1491ab47db62125963d80480d4ca91
Oliver Levay <oliver.levay@hotmail.com>
2022-10-22 00:30:36 +0200

    update the way datasources are created in testserver

--- backend/services/core/tests/util.ts
+++ backend/services/core/tests/util.ts
@@ -2,12 +2,15 @@ import { ApolloServer } from 'apollo-server';
 
 import createApolloServer from '../src/server';
 
-import dataSources, { DataSources } from '../src/datasources';
+import datasources, { DataSources } from '../src/datasources';
 
 type TestServerReturn = {server: ApolloServer, context: any, dataSources: DataSources}
 
-export default (context?: any): TestServerReturn => ({
-  server: createApolloServer(),
-  context,
-  dataSources: dataSources(),
-});
+export default (context?: any): TestServerReturn => {
+  const dataSources = datasources();
+  return ({
+    server: createApolloServer(),
+    context,
+    dataSources,
+  });
+};
