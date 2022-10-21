306a4ae0e5faf471b0ed8673fc7091c371fbfb64
Oliver Levay <oliver.levay@hotmail.com>
2022-10-21 10:44:53 +0200

    Fix gitsha docker mistake

--- frontend/pages/api/gitsha.ts
+++ frontend/pages/api/gitsha.ts
@@ -3,9 +3,9 @@ import { readFileSync } from 'fs';
 import { SHAResult } from '~/types/SHAResult';
 
 export default function handler(req, res) {
-  let sha = readFileSync('.git/HEAD').toString().trim();
+  let sha = readFileSync('../.git/HEAD').toString().trim();
   if (sha.indexOf(':') !== -1) {
-    sha = readFileSync(`.git/${sha.substring(5)}`).toString().trim();
+    sha = readFileSync(`../.git/${sha.substring(5)}`).toString().trim();
   }
 
   const result: SHAResult = {
