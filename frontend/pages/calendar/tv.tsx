29883eb9072ecfafba0b227eb8d64567559ad8f0
Oliver Levay <oliver.levay@hotmail.com>
2022-10-21 11:59:14 +0200

    Add query customization to tv pages (#530)
    * Add query customization to tv pages
    
    * Fix events list sorting

--- frontend/pages/calendar/tv.tsx
+++ frontend/pages/calendar/tv.tsx
@@ -1,36 +1,18 @@
-import React from 'react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { useTranslation } from 'next-i18next';
-import {
-  Grid,
-  Paper,
-} from '@mui/material';
-import BigCalendar from '~/components/Calendar/BigCalendar';
+import TVWrapper from '~/components/TV/TVWrapper';
+import CalendarPage from '.';
 
-function CalendarPage() {
-  const { t } = useTranslation('common');
-
+function CalendarTVPage() {
   return (
-    <Grid
-      container
-      spacing={3}
-      direction="row"
-      justifyContent="center"
-      alignItems="flex-start"
-    >
-      <Grid item xs={12} sm={12} md={12} lg={12}>
-        <h2>{t('calendar')}</h2>
-        <Paper style={{ padding: '0.5rem' }}>
-          <BigCalendar />
-        </Paper>
-      </Grid>
-    </Grid>
+    <TVWrapper>
+      <CalendarPage />
+    </TVWrapper>
   );
 }
 
-CalendarPage.tv = true;
+CalendarTVPage.tv = true;
 
-export default CalendarPage;
+export default CalendarTVPage;
 
 export const getStaticProps = async ({ locale }) => ({
   props: {
