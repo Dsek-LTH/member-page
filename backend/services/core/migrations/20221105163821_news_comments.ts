694be91da8345d2266879a7a31a73846a7c5bdfc
Oliver Levay <oliver.levay@hotmail.com>
2022-11-05 16:49:51 +0100

    :card_file_box: Add article_comments table

---
+++ backend/services/core/migrations/20221105163821_news_comments.ts
@@ -0,0 +1,25 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('article_comments', (t) => {
+    t.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+    t.uuid('article_id')
+      .unsigned()
+      .notNullable()
+      .references('articles.id')
+      .onDelete('CASCADE')
+      .comment('The article id');
+    t.uuid('member_id')
+      .unsigned()
+      .notNullable()
+      .references('members.id')
+      .onDelete('CASCADE')
+      .comment('The member id');
+    t.comment('A relation table for likes on news articles');
+    t.string('content').comment('The actual comment');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.raw('DROP TABLE article_likes CASCADE');
+}
