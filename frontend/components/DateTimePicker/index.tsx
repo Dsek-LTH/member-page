a73e98b27c6d591369ce47a9c96aff095cced13f
Oliver Levay <oliver.levay@hotmail.com>
2022-11-10 15:57:38 +0100

    :alien: Migrate from @mui/labs to @mui/x-date-pickers

--- frontend/components/DateTimePicker/index.tsx
+++ frontend/components/DateTimePicker/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import AdapterLuxon from '@mui/lab/AdapterLuxon';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { useTranslation } from 'next-i18next';
-import { DatePicker, TimePicker } from '@mui/lab';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { Stack, TextField } from '@mui/material';
 import { DateTime } from 'luxon';
 
@@ -20,7 +21,6 @@ export default function DateTimePicker({
   dateLabel,
 }: DateTimePickerProps) {
   const { i18n } = useTranslation(['common']);
-
   return (
     <LocalizationProvider dateAdapter={AdapterLuxon} locale={i18n.language}>
       <Stack spacing={2} direction="row">
