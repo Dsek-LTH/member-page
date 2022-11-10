b4603a9172d5d419b359e6de059c50f4192ca8aa
ludviggierup <35451091+ludviggierup@users.noreply.github.com>
2022-11-10 18:34:08 +0100

    :lipstick: Fixed Committee width not working on mobile (#614)

--- frontend/components/Committees/CommitteesList.tsx
+++ frontend/components/Committees/CommitteesList.tsx
@@ -1,10 +1,20 @@
-import { Paper, Stack } from '@mui/material';
+import { Paper, Stack, Link as MuiLink } from '@mui/material';
 import { styled } from '@mui/system';
 import React from 'react';
-import Link from '~/components/Link';
+import Link from 'next/link';
 import useCommittees from '~/hooks/useCommittees';
 import routes from '~/routes';
 import CommitteeIcon from './CommitteeIcon';
+
+const Card = styled(Stack)(({ theme }) => `
+  display: flex;
+  width: 100%;
+  margin: 1rem;
+  ${theme.breakpoints.up('md')} {
+    max-width: 18rem;
+  }
+  width: 100%;
+`);
 
 const Committees = styled(Stack)`
   display: flex;
@@ -15,34 +25,31 @@ const Committees = styled(Stack)`
   margin-right: -1rem;
 `;
 
-const Committee = styled(Paper)(
-  ({ theme }) => `
+const Committee = styled(Paper)`
   display: flex;
   padding: 2rem;
   min-width: 18rem;
-  ${theme.breakpoints.up('md')} {
-    max-width: 18rem;
-  }
-  width: 100%;
-  margin: 1rem;
-`,
-);
+`;
 
 function CommitteesList() {
   const { committees } = useCommittees();
   return (
     <Committees>
       {committees.map((committee) => (
-        <Link href={routes.committeePage(committee.id)} key={committee.id}>
-          <Committee>
-            <CommitteeIcon
-              name={committee.name}
-              color="primary"
-              style={{ marginRight: '1rem' }}
-            />
-            {committee.name}
-          </Committee>
-        </Link>
+        <Card key={committee.id}>
+          <Link href={routes.committeePage(committee.id)} passHref>
+            <MuiLink>
+              <Committee>
+                <CommitteeIcon
+                  name={committee.name}
+                  color="primary"
+                  style={{ marginRight: '1rem' }}
+                />
+                {committee.name}
+              </Committee>
+            </MuiLink>
+          </Link>
+        </Card>
       ))}
     </Committees>
   );
