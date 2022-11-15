c787de095b4847ed2f9fd3d67eb483e4f8cc57dd
Oliver Levay <oliver.levay@hotmail.com>
2022-11-15 13:17:45 +0100

    :bookmark: v1.0.2

--- backend/services/core/setup_test_db.sh
+++ backend/services/core/setup_test_db.sh
@@ -1,4 +1,3 @@
 docker run --name postgres-test -p 9999:5432 -e POSTGRES_USER=user -e POSTGRES_PASSWORD=password -e POSTGRES_DB=dsek -d postgres:14-alpine
-cd services/core
 npm install
 npm run migrate-test
