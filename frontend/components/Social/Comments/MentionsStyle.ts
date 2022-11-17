65378ab66ee1309617a5dac5e5c261f5f7b08690
Lucas Boberg <lucaspboberg@gmail.com>
2022-11-17 21:44:13 +0100

    :bug: Fix borked mentions on light theme (#703)

--- frontend/components/Social/Comments/MentionsStyle.ts
+++ frontend/components/Social/Comments/MentionsStyle.ts
@@ -1,57 +1,61 @@
-export default {
-  width: '85%',
+export default function mentionsStyle(darkMode: boolean) {
+  return (
+    {
+      width: '85%',
 
-  control: {
-    backgroundColor: 'transparent',
-    fontSize: '1rem',
-    fontWeight: 'normal',
-    color: 'inherit',
-  },
-
-  '&multiLine': {
-    control: {
-      fontFamily: 'Roboto',
-      minHeight: '1rem',
-      color: 'inherit',
-    },
-    highlighter: {
-      padding: 9,
-      border: '1px solid transparent',
-    },
-    input: {
-      padding: '0.5rem 1rem',
-      borderRadius: '2rem',
-      color: 'inherit',
-    },
-  },
-
-  '&singleLine': {
-    display: 'inline-block',
-    width: 180,
-
-    highlighter: {
-      padding: 1,
-      border: '2px inset transparent',
-    },
-    input: {
-      padding: 1,
-      border: '2px inset',
-      color: 'inherit',
-    },
-  },
-
-  suggestions: {
-    list: {
-      backgroundColor: 'black',
-      border: '1px solid rgba(0,0,0,0.15)',
-      fontSize: 14,
-    },
-    item: {
-      padding: '5px 15px',
-      borderBottom: '1px solid rgba(0,0,0,0.15)',
-      '&focused': {
-        backgroundColor: 'pink',
+      control: {
+        backgroundColor: 'transparent',
+        fontSize: '1rem',
+        fontWeight: 'normal',
+        color: 'inherit',
       },
-    },
-  },
-};
+
+      '&multiLine': {
+        control: {
+          fontFamily: 'Roboto',
+          minHeight: '1rem',
+          color: 'inherit',
+        },
+        highlighter: {
+          padding: 9,
+          border: '1px solid transparent',
+        },
+        input: {
+          padding: '0.5rem 1rem',
+          borderRadius: '2rem',
+          color: 'inherit',
+        },
+      },
+
+      '&singleLine': {
+        display: 'inline-block',
+        width: 180,
+
+        highlighter: {
+          padding: 1,
+          border: '2px inset transparent',
+        },
+        input: {
+          padding: 1,
+          border: '2px inset',
+          color: 'inherit',
+        },
+      },
+
+      suggestions: {
+        list: {
+          backgroundColor: darkMode ? '#1e1e1e' : '#fff',
+          border: '1px solid rgba(0,0,0,0.15)',
+          fontSize: 14,
+        },
+        item: {
+          padding: '5px 15px',
+          borderBottom: '1px solid rgba(0,0,0,0.15)',
+          '&focused': {
+            backgroundColor: '#F280A1',
+          },
+        },
+      },
+    }
+  );
+}
