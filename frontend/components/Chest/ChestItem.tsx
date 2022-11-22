83501eeb28a6357a611f360a4d1bb0358c51d1b0
Oliver Levay <oliver.levay@hotmail.com>
2022-11-22 12:54:10 +0100

    :sparkles: Add frontend for consuming items

--- frontend/components/Chest/ChestItem.tsx
+++ frontend/components/Chest/ChestItem.tsx
@@ -5,12 +5,15 @@ import {
   Typography,
   Button,
 } from '@mui/material';
-import { MyChestQuery } from '~/generated/graphql';
+import { MyChestQuery, useConsumeItemMutation } from '~/generated/graphql';
 
 export default function ChestItem({ item }: {
   item: MyChestQuery['chest']['items'][number]
 }) {
   const consumed = !!item.consumedAt;
+  const [consumeItemMutation] = useConsumeItemMutation({
+    variables: { itemId: item.id },
+  });
   return (
     <Paper key={item.id} sx={{ marginBottom: '1rem', width: 'fit-content' }}>
       <Stack direction="row" alignItems="center" spacing={2} padding={2} sx={{ width: 'fit-content' }}>
@@ -19,7 +22,16 @@ export default function ChestItem({ item }: {
           {item.name}
           {item.variant ? `: ${item.variant}` : ''}
         </Typography>
-        {!consumed && <Button variant="contained">Förbruka</Button>}
+        {!consumed && (
+        <Button
+          onClick={() => {
+            consumeItemMutation();
+          }}
+          variant="contained"
+        >
+          Förbruka
+        </Button>
+        )}
         {consumed && (
         <Typography>
           Förbrukad:
