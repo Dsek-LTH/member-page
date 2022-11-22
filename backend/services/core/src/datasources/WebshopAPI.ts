9fe4d96d9e54102ca1d838205b0117e87402665e
Oliver Levay <oliver.levay@hotmail.com>
2022-11-22 10:34:01 +0100

    :children_crossing: Delay rerouting to chest so that the user can see the success message

--- backend/services/core/src/datasources/WebshopAPI.ts
+++ backend/services/core/src/datasources/WebshopAPI.ts
@@ -486,6 +486,7 @@ export default class WebshopAPI extends dbUtils.KnexDataSource {
         await this.knex<sql.Order>(TABLE.ORDER).where({ id: order.id }).del();
         await this.knex<sql.Payment>(TABLE.PAYMENT).where({ id: payment.id }).update({
           payment_status: 'ERROR',
+          updated_at: new Date(),
         });
         throw new Error(error);
       }
