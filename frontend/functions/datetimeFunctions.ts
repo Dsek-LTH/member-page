931f8729cb27ade0c346560cde903c36c0653f82
Hien Truong <hien.truong2002.ht@gmail.com>
2022-11-24 20:18:11 +0100

    Fixed: if the post was posted more than a week ago, date shows

--- frontend/functions/datetimeFunctions.ts
+++ frontend/functions/datetimeFunctions.ts
@@ -14,15 +14,8 @@ const units: Intl.RelativeTimeFormatUnit[] = [
 export const timeAgo = (dateTime: DateTime) => {
   const relDate = dateTime.toRelative();
   const diff = dateTime.diffNow().shiftTo(...units);
-  if (diff.months <= -1 || diff.years <= -1) {
+  if (diff.weeks <= -1) {
     return dateTime.toLocaleString(DateTime.DATE_SHORT);
   }
   return relDate;
-
-  const unit = units.find((u) => diff.get(u) !== 0) || 'second';
-
-  const relativeFormatter = new Intl.RelativeTimeFormat('en', {
-    numeric: 'auto',
-  });
-  return relativeFormatter.format(Math.trunc(diff.as(unit)), unit);
 };
