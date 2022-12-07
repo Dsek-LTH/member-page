3552c85381bc1eda2cf7c1bcf28aaab4a44471af
Oliver Levay <oliver.levay@hotmail.com>
2022-12-07 04:31:39 +0100

    :children_crossing: Put create event button in a better place

--- frontend/pages/events/index.tsx
+++ frontend/pages/events/index.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { useTranslation } from 'next-i18next';
 import { Button, Stack } from '@mui/material';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import AddIcon from '@mui/icons-material/Add';
 import { useRouter } from 'next/router';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import EventSet from '~/components/Calendar/UpcomingEventSet';
 import EventSearchInput from '~/components/Calendar/EventSearchInput';
 import routes from '~/routes';
 import Link from '~/components/Link';
+import { useApiAccess } from '~/providers/ApiAccessProvider';
 
 export default function EventsPage() {
   const router = useRouter();
   const { t } = useTranslation('common');
-
+  const { hasAccess } = useApiAccess();
   return (
     <Stack>
       <h2>{t('upcomingEvents')}</h2>
-      <Stack marginBottom="1rem" direction="row" spacing={2}>
+      <Stack marginBottom="1rem" direction="row" flexWrap="wrap" spacing={2}>
         <Link href={routes.calendar}>
           <Button>
             {t('calendar')}
@@ -26,6 +28,14 @@ export default function EventsPage() {
             {t('passedEvents')}
           </Button>
         </Link>
+        {hasAccess('event:create') && (
+          <Link href={routes.createEvent}>
+            <Button variant="outlined">
+              {t('event:create_event')}
+              <AddIcon style={{ marginLeft: '0.25rem' }} />
+            </Button>
+          </Link>
+        )}
       </Stack>
       <div style={{ marginBottom: '0.5rem' }}>
         <EventSearchInput onSelect={(slug, id) => router.push(routes.event(slug || id))} />
