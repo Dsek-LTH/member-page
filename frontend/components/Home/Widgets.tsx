0ed689ca0c2998ca6d6b971a75cb4bd92bc8d049
Oliver Levay <oliver.levay@hotmail.com>
2022-12-11 12:03:28 +0100

    :bug: Fix widget visual bug

--- frontend/components/Home/Widgets.tsx
+++ frontend/components/Home/Widgets.tsx
@@ -8,17 +8,17 @@ import DisplayMeeting from './DisplayMeeting';
 export default function Widgets() {
   return (
     <Stack direction={{ xs: 'column', md: 'row' }} spacing={{ xs: 2, md: 5 }} sx={{ width: '100%' }} justifyContent="center">
-      <Stack width="100%" spacing={2} alignItems="center">
+      <Stack width="100%" spacing={2}>
         <Typography variant="h4" color="primary">Senaste nyheterna</Typography>
         <ArticleSet articlesPerPage={5} />
         <Link href={routes.news}>Fler nyheter</Link>
       </Stack>
-      <Stack width="100%" spacing={2} alignItems="center">
+      <Stack width="100%" spacing={2}>
         <Typography variant="h4" color="secondary">Kommande evenemang</Typography>
         <EventSet perPage={4} />
         <Link href={routes.calendar}>Till kalendern</Link>
       </Stack>
-      <Stack width="100%" spacing={2} alignItems="center">
+      <Stack width="100%" spacing={2}>
         <Typography variant="h4" color="primary">Senaste mötet</Typography>
         <DisplayMeeting index={1} />
         <Typography variant="h4" color="primary">Nästa möte</Typography>
