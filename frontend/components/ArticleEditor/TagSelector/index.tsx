3b845658a5c8314318d22536b3b3937fb2686b30
Ludvig <ludvig.svedberg@mac.com>
2023-03-20 15:32:10 +0100

    Use a select instead of autocomplete for tag selection on phones

--- frontend/components/ArticleEditor/TagSelector/index.tsx
+++ frontend/components/ArticleEditor/TagSelector/index.tsx
@@ -1,4 +1,6 @@
-import { Autocomplete, TextField } from '@mui/material';
+import {
+  Autocomplete, FormControl, InputLabel, MenuItem, Select, TextField, useMediaQuery, useTheme,
+} from '@mui/material';
 import { useTranslation } from 'next-i18next';
 import selectTranslation from '~/functions/selectTranslation';
 import { useGetTagsQuery } from '~/generated/graphql';
@@ -12,11 +14,52 @@ type Props = {
 function TagSelector({ currentlySelected, onChange }: Props) {
   const { data } = useGetTagsQuery();
   const tags = data?.tags || [];
+  const theme = useTheme();
+  const isPhone = useMediaQuery(`(max-width: ${theme.breakpoints.values.sm}px)`);
 
-  const handleChange = (_, value) => {
-    onChange(value.map((tag) => tag.id));
-  };
   const { t, i18n } = useTranslation('news');
+
+  if (isPhone) {
+    return (
+      <FormControl fullWidth>
+        <InputLabel id="tag-selector-label">{t('tags')}</InputLabel>
+        <Select
+          id="tag-selector"
+          labelId="tag-selector-label"
+          label={t('tags')}
+          multiple
+          value={tags.filter((tag) => currentlySelected.includes(tag.id)).map((tag) => tag.id)}
+          onChange={(event) => {
+            const { value } = event.target;
+            onChange(typeof value === 'string' ? value.split(',') : value);
+          }}
+          renderValue={(values) => {
+            const TagComponents = values.map((value) => {
+              const tagValue = tags.find((tag) => tag.id === value);
+              if (!tagValue) return value;
+              return <Tag key={value} tag={tagValue} />;
+            });
+            return TagComponents;
+          }}
+          MenuProps={{
+            anchorOrigin: { vertical: 'bottom', horizontal: 'center' },
+            PaperProps: {
+              style: {
+                maxHeight: 400,
+              },
+            },
+          }}
+        >
+          {tags.map((tag) => (
+            <MenuItem value={tag.id} key={tag.id}>
+              {selectTranslation(i18n, tag.name, tag.nameEn)}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+
+    );
+  }
 
   return (
     <Autocomplete
@@ -25,13 +68,18 @@ function TagSelector({ currentlySelected, onChange }: Props) {
       id="tag-selector"
       options={tags}
       getOptionLabel={(tag) => (typeof tag === 'string' ? tag : selectTranslation(i18n, tag.name, tag.nameEn))}
-      onChange={handleChange}
+      onChange={(_, value: any) => {
+        onChange(value.map((tag) => tag.id));
+      }}
       renderTags={(tagValue, getTagProps) =>
         tagValue.map((option, index) => (
           <Tag key={option.id} tag={option} {...getTagProps({ index })} />
         ))}
       renderInput={(params) => (
-        <TextField {...params} label={t('tags')} />
+        <TextField
+          {...params}
+          label={t('tags')}
+        />
       )}
     />
   );
