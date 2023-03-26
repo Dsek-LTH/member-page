ff35bbb1686851336bf377fe0933244f2062a141
Samuel Högfeldt <samuel.hogfeldt.2446@student.lu.se>
2023-03-26 21:59:00 +0200

    :art: Some productInfo is now displayed
    First draft of product page getting data from query and displaying it with the help of MUI grid

--- frontend/pages/webshop/product/[id].tsx
+++ frontend/pages/webshop/product/[id].tsx
@@ -1,16 +1,37 @@
+import { Grid } from '@mui/material';
 import { useRouter } from 'next/router';
+import { useProductQuery } from '~/generated/graphql';
 import routes from '~/routes';
 
 export default function ProductPage() {
     const router = useRouter();
     const { id } = router.query;
+    const {data, refetch} = useProductQuery({variables: {id: id as string}})
     //const products = data?.products || [];
     //product = useProductQuery(id)
 
     return(
-    <h2>
-        Insert product page
-    </h2>)
+    <div>
+        <Grid container spacing = {1}>
+            <Grid item xs = {8}>
+                <img 
+                src = {data?.product?.imageUrl}
+                />
+            </Grid>
+            <Grid item xs = {4}>
+                <Grid item xs = {6}>
+                    <h1>
+                        {data?.product?.name}
+                    </h1>
+                </Grid>
+                <Grid item xs = {6}>
+                    <h3>
+                        {data?.product?.price} kr
+                    </h3>
+                </Grid>
+            </Grid>
+        </Grid>
+    </div>)
 
     //allt gemensamt
     //Component bereonde på kategori 
