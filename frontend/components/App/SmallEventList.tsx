90e01109d4852653f57580bbb82d322b794f4879
Ludvig Svedberg <ludvig.svedberg@mac.com>
2023-03-28 01:35:13 +0200

    Style and add skeletons to app home page

--- frontend/components/App/SmallEventList.tsx
+++ frontend/components/App/SmallEventList.tsx
@@ -2,12 +2,13 @@ import { Stack, Typography } from '@mui/material';
 import { DateTime } from 'luxon';
 import { useTranslation } from 'react-i18next';
 import { SmallEventCard } from '~/components/Calendar/EventCard';
+import ArticleSkeleton, { SmallArticleSkeleton } from '~/components/News/articleSkeleton';
 import { sortByStartDateAscending } from '~/functions/sortByDate';
 import { useEventsQuery } from '~/generated/graphql';
 
 const now = DateTime.now();
 function SmallEventList() {
-  const { data } = useEventsQuery({
+  const { data, loading } = useEventsQuery({
     variables:
      {
        start_datetime: now.minus({ month: 1 }),
@@ -24,7 +25,8 @@ function SmallEventList() {
 
   return (
     <Stack gap={1}>
-      <Typography variant="h6" fontWeight="bold">{t('upcomingEvents')}</Typography>
+      <h2 style={{marginBottom: 0}}>{t('upcomingEvents')}</h2>
+      {loading && ([0,1].map(() => <SmallArticleSkeleton />))}
       {events.map((event) => (
         <SmallEventCard
           key={event.id}
--- frontend/components/News/articleSkeleton.tsx
+++ frontend/components/News/articleSkeleton.tsx
@@ -6,15 +6,34 @@ import Typography from '@mui/material/Typography';
 import { Stack } from '@mui/material';
 import articleStyles from './articleStyles';
 
+export function SmallArticleSkeleton() {
+  const classes = articleStyles();
+
+  return (
+    <Paper sx={{p:2}}>
+      <Stack
+        direction="row"
+        alignItems="stretch"
+        style={{ position: 'relative' }}
+        gap={1}
+      >
+        <Skeleton variant='circular' width={48} height={48}/>
+        <Stack sx={{flex: 1}} justifyContent="space-between">
+          <Skeleton height={20}/>
+          <Skeleton />
+        </Stack>
+      </Stack>
+    </Paper>
+  );
+}
+
 export default function ArticleSkeleton({ fullArticle }: { fullArticle?: boolean }) {
   const classes = articleStyles();
 
   return (
     <Paper className={classes.article}>
-      <Stack
-        direction="row"
-        alignItems="flex-start"
-        style={{ position: 'relative' }}
+      <Grid
+        container
       >
         <Grid item xs={12} md={12} lg={7} style={{ minHeight: fullArticle ? '500px' : '200px' }}>
           <Typography variant="h3">
@@ -24,7 +43,7 @@ export default function ArticleSkeleton({ fullArticle }: { fullArticle?: boolean
           <Skeleton />
         </Grid>
 
-      </Stack>
+      </Grid>
     </Paper>
   );
 }
