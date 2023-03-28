5658fac6e698c864a992daa5a677ea65f675add8
Ludvig <ludvig.svedberg@mac.com>
2023-03-28 22:06:00 +0200

    Add and fix tests for position emailAlias changes

--- backend/services/core/tests/core/mandate.spec.ts
+++ backend/services/core/tests/core/mandate.spec.ts
@@ -17,10 +17,10 @@ const sandbox = chai.spy.sandbox();
 
 const positions: sql.Position[] = [
   {
-    id: 'dsek.infu.dwww.medlem', name: 'DWWW-medlem', name_en: 'DWWW member', description: '', description_en: '', committee_id: null, active: true, board_member: false, email: '',
+    id: 'dsek.infu.dwww.medlem', name: 'DWWW-medlem', name_en: 'DWWW member', description: '', description_en: '', committee_id: null, active: true, board_member: false,
   },
   {
-    id: 'dsek.km.mastare', name: 'Källarmästare', name_en: 'Head of Facilities', description: '', description_en: '', committee_id: null, active: true, board_member: false, email: 'kallarmastare@dsek.se',
+    id: 'dsek.km.mastare', name: 'Källarmästare', name_en: 'Head of Facilities', description: '', description_en: '', committee_id: null, active: true, board_member: false,
   },
 ];
 
