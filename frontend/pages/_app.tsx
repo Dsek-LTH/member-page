1ae5aea912bbdcbff7cd4c8d2136d9babf0745c8
Ludvig <ludvig.svedberg@mac.com>
2023-04-07 10:52:32 +0200

    Make site non-zoomable when it's a native app

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { appWithTranslation } from 'next-i18next';
 import { AppProps } from 'next/app';
 import { useEffect } from 'react';
+import Head from 'next/head';
 import Layout from '~/components/Layout';
 import { ApiAccessProvider } from '~/providers/ApiAccessProvider';
 import { DialogProvider } from '~/providers/DialogProvider';
@@ -25,28 +26,33 @@ AppProps & { Component: any, pageProps: any }) {
   const isTV = Component?.tv;
 
   return (
-    <NativeAppProvider isNativeApp={pageProps.isNativeApp}>
-      <LoginProvider session={session} apolloCache={pageProps.apolloCache}>
-        <ThemeProvider>
-          <UserProvider>
-            <ApiAccessProvider>
-              <SnackbarProvider>
-                <DialogProvider>
-                  {!isTV && (
-                  <Layout>
+    <>
+      <Head>
+        {pageProps.isNativeApp && <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />}
+      </Head>
+      <NativeAppProvider isNativeApp={pageProps.isNativeApp}>
+        <LoginProvider session={session} apolloCache={pageProps.apolloCache}>
+          <ThemeProvider>
+            <UserProvider>
+              <ApiAccessProvider>
+                <SnackbarProvider>
+                  <DialogProvider>
+                    {!isTV && (
+                    <Layout>
+                      <Component {...pageProps} />
+                    </Layout>
+                    )}
+                    {isTV && (
                     <Component {...pageProps} />
-                  </Layout>
-                  )}
-                  {isTV && (
-                  <Component {...pageProps} />
-                  )}
-                </DialogProvider>
-              </SnackbarProvider>
-            </ApiAccessProvider>
-          </UserProvider>
-        </ThemeProvider>
-      </LoginProvider>
-    </NativeAppProvider>
+                    )}
+                  </DialogProvider>
+                </SnackbarProvider>
+              </ApiAccessProvider>
+            </UserProvider>
+          </ThemeProvider>
+        </LoginProvider>
+      </NativeAppProvider>
+    </>
   );
 }
 
