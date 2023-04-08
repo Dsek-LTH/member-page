11c6c2f9ec77363713f636180886e74d609d5e18
Ludvig Svedberg <ludvig.svedberg@mac.com>
2023-04-09 01:09:02 +0200

    Fix meeting loading forever and translation not working

--- frontend/components/Home/DisplayMeeting.tsx
+++ frontend/components/Home/DisplayMeeting.tsx
@@ -6,13 +6,14 @@ import processFilesData from '../Documents/proccessFilesData';
 const thisYear = new Date().getFullYear().toString();
 
 export default function DisplayMeeting({ index = 0 }) {
-  const { data } = useFilesQuery({ variables: { bucket: 'documents', prefix: `public/${thisYear}`, recursive: true } });
+  const { data, loading } = useFilesQuery({ variables: { bucket: 'documents', prefix: `public/${thisYear}`, recursive: true } });
   const meetings = processFilesData(thisYear, data?.files || [])
     .filter((m) => m.modDate).sort((a, b) => (a.modDate < b.modDate ? 1 : -1));
   // sort meetings after date
 
   const latestMeeting = meetings[index];
-  if (!latestMeeting) return <CircularProgress />;
+  if (loading) return <CircularProgress />;
+  if (!latestMeeting) return null;
   return (
     <MeetingComponent meeting={latestMeeting} />
   );
