033e5a4c8482a1ef6818531ee6c03227dcdf0a8a
Oliver Levay <oliver.levay@hotmail.com>
2023-05-08 22:16:19 +0200

    :lock: Restrict traversal

--- frontend/pages/api/pdf/[pathName].ts
+++ frontend/pages/api/pdf/[pathName].ts
@@ -3,11 +3,15 @@ import fetch from 'node-fetch';
 
 export default async function passThroughPdf(req: NextApiRequest, res: NextApiResponse) {
   const pathName = req.query.pathName as string;
-  const fileName = pathName.split('/').pop();
-  const url = new URL(pathName, 'https://github.com/Dsek-LTH/');
-  const response = await fetch(url);
-  const buffer = await response.buffer();
-  res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Disposition', `inline; filename="${fileName}"`);
-  res.send(buffer);
+  if (pathName.includes('..')) {
+    res.status(400).send('Bad request');
+  } else {
+    const fileName = pathName.split('/').pop();
+    const url = new URL(pathName, 'https://github.com/Dsek-LTH/');
+    const response = await fetch(url);
+    const buffer = await response.buffer();
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', `inline; filename="${fileName}"`);
+    res.send(buffer);
+  }
 }
