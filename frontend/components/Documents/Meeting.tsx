8ffe86e10ab2d5fd2552754f4306cd9b4f0d84d9
Ludvig <ludvig.svedberg@mac.com>
2023-05-08 20:35:51 +0200

    Make files into a grid instead of purely list

--- frontend/components/Documents/Meeting.tsx
+++ frontend/components/Documents/Meeting.tsx
@@ -1,5 +1,6 @@
 import ArticleIcon from '@mui/icons-material/Article';
-import {
+import
+{
   Button, Paper,
 } from '@mui/material';
 import { Box, styled } from '@mui/system';
@@ -10,37 +11,39 @@ const MeetingPaper = styled(Paper)`
   flex-direction: column;
   padding: 1rem;
   margin-top: 1rem;
-  max-width: 40rem;
   overflow-wrap: anywhere;
-`;
-
-const File = styled(Box)`
-  margin-top: 1rem;
 `;
 
 export default function MeetingComponent({ meeting }: { meeting: Meeting }) {
   return (
     <MeetingPaper key={meeting.title}>
       <h2 style={{ margin: 0 }}>{meeting.title}</h2>
-      {meeting.files.map((file) => (
-        <File key={`file-${file.name}`}>
+      <Box sx={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
+        gap: 2,
+      }}
+      >
+        {meeting.files.map((file) => (
           <Button
             variant="contained"
             target="_blank"
             rel="noopener noreferrer"
             href={file.thumbnailUrl}
+            key={`file-${file.name}`}
             download
-            fullWidth
             sx={{
               overflowWrap: 'anywhere',
+              textOverflow: 'ellipsis',
+              overflow: 'hidden',
               justifyContent: 'flex-start',
             }}
           >
             <ArticleIcon style={{ marginRight: '0.5rem' }} />
-            {file.name}
+            {file.name.replaceAll('_', ' ')}
           </Button>
-        </File>
-      ))}
+        ))}
+      </Box>
     </MeetingPaper>
   );
 }
