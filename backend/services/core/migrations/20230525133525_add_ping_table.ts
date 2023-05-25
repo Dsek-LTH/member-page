b944fc4e7073ee93801217a2b732bf0c2a8ebd8e
Ludvig <ludvig.svedberg@mac.com>
2023-05-25 16:23:59 +0200

    Add migration for pings table

---
+++ backend/services/core/migrations/20230525133525_add_ping_table.ts
@@ -0,0 +1,20 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('pings', (t) => {
+    t.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+    t.uuid('from_member').unsigned().unique().nullable()
+      .references('members.id')
+      .onDelete('CASCADE')
+      .comment('The member who sent the ping');
+    t.uuid('to_member').unsigned().unique().nullable()
+      .references('members.id')
+      .onDelete('CASCADE')
+      .comment('The member who sent the ping');
+    t.timestamp('created_at').defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('pings');
+}
