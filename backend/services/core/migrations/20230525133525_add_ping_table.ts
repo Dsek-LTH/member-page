41745d4b8badbf3089406311ab63bf6d41fc7b4f
Ludvig <ludvig.svedberg@mac.com>
2023-05-25 16:29:10 +0200

    Fix wrong unique constraint in migration

--- backend/services/core/migrations/20230525133525_add_ping_table.ts
+++ backend/services/core/migrations/20230525133525_add_ping_table.ts
@@ -3,11 +3,11 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('pings', (t) => {
     t.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
-    t.uuid('from_member').unsigned().unique().nullable()
+    t.uuid('from_member').unsigned()
       .references('members.id')
       .onDelete('CASCADE')
       .comment('The member who sent the ping');
-    t.uuid('to_member').unsigned().unique().nullable()
+    t.uuid('to_member').unsigned()
       .references('members.id')
       .onDelete('CASCADE')
       .comment('The member who sent the ping');
