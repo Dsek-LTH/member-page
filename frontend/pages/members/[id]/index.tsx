8cdeee90b742d2e44d93a8c4d4eb11cae5f6060e
Ludvig <ludvig.svedberg@mac.com>
2023-05-26 15:37:39 +0200

    Re-style member page buttons and add link to /pings

--- frontend/pages/members/[id]/index.tsx
+++ frontend/pages/members/[id]/index.tsx
@@ -5,6 +5,9 @@ import {
 import { i18n, useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
 import { useContext } from 'react';
+import EditIcon from '@mui/icons-material/Edit';
+import AdsClickIcon from '@mui/icons-material/AdsClick';
+import PortraitIcon from '@mui/icons-material/Portrait';
 import Link from '~/components/Link';
 import Member from '~/components/Members/Member';
 import MemberSkeleton from '~/components/Members/MemberSkeleton';
@@ -17,8 +20,7 @@ import { useMemberPageQuery, usePingMemberMutation } from '~/generated/graphql';
 import { useApiAccess } from '~/providers/ApiAccessProvider';
 import { useSetPageName } from '~/providers/PageNameProvider';
 import UserContext from '~/providers/UserProvider';
-import routes from '~/routes';
-import commonPageStyles from '~/styles/commonPageStyles';
+import routes from '~/routes'; import commonPageStyles from '~/styles/commonPageStyles';
 
 export default function MemberPage() {
   const router = useRouter();
@@ -65,35 +67,46 @@ export default function MemberPage() {
         <Member
           member={member}
         />
-        <Stack direction="row" spacing={2} marginTop={1}>
-          {hasAccess('core:member:ping') && member.id !== user?.id && (
+        <Stack direction="row" gap={2} marginTop={4} justifyContent="space-between" flexWrap="wrap">
+          {(member.id === user?.id || hasAccess('core:member:update')) && (
+          <Stack direction="row" gap={2} flexWrap="wrap">
+            <Link href={routes.editMember(member.id)}>
+              <Button variant="contained" startIcon={<EditIcon />}>
+                {t('member:editMember')}
+              </Button>
+            </Link>
+            <Link href={routes.changeProfilePicture(member.id)}>
+              <Button startIcon={<PortraitIcon />}>
+                {t('member:changeProfilePicture')}
+              </Button>
+            </Link>
+          </Stack>
+          )}
+          {hasAccess('core:member:ping') && (member.id !== user?.id ? (
             <Tooltip title={!member.canPing ? t('member:pingWaitForResponse') : t('member:pingTooltip')}>
               <Box>
                 <Button
-                  variant="contained"
+                  variant="outlined"
                   disabled={!member.canPing}
                   onClick={async () => {
                     await ping();
                     refetch();
                   }}
+                  startIcon={<AdsClickIcon />}
                 >
                   {t('member:ping')}
                 </Button>
               </Box>
             </Tooltip>
-          )}
-          {(member.id === user?.id || hasAccess('core:member:update')) && (
-          <>
-            <Link href={routes.editMember(member.id)}>
-              <Button>{t('member:editMember')}</Button>
-            </Link>
-            <Link href={routes.changeProfilePicture(member.id)}>
-              <Button>
-                {t('member:changeProfilePicture')}
+          ) : (
+            <Link href={routes.pings}>
+              <Button
+                startIcon={<AdsClickIcon />}
+              >
+                {t('member:myPings')}
               </Button>
             </Link>
-          </>
-          )}
+          ))}
         </Stack>
       </Paper>
     </NoTitleLayout>
