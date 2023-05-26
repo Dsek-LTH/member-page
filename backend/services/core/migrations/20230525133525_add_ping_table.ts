9c548f8a8d3e9628bfd3742fa12aa2504f938d34
Ludvig <ludvig.svedberg@mac.com>
2023-05-26 15:27:21 +0200

    Fix comment on migration

--- backend/services/core/migrations/20230525133525_add_ping_table.ts
+++ backend/services/core/migrations/20230525133525_add_ping_table.ts
@@ -10,7 +10,7 @@ export async function up(knex: Knex): Promise<void> {
     t.uuid('to_member').unsigned()
       .references('members.id')
       .onDelete('CASCADE')
-      .comment('The member who sent the initial ping');
+      .comment('The member who received the initial ping');
     t.unique(['from_member', 'to_member']);
     t.timestamp('from_sent_at').notNullable().defaultTo(knex.fn.now()).comment('When the last ping was sent from the from_member');
     t.timestamp('to_sent_at').comment('When the last ping was sent from the to_member');
