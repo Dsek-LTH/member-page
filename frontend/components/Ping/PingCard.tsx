5b4101a8e75d9397aada5ac46a9599d40039335d
Ludvig <ludvig.svedberg@mac.com>
2023-05-26 17:22:57 +0200

    Fix design of ping card on mobile device

--- frontend/components/Ping/PingCard.tsx
+++ frontend/components/Ping/PingCard.tsx
@@ -1,6 +1,7 @@
 import
 {
   Avatar,
+  Box,
   Button,
   Paper, Stack, Typography,
 } from '@mui/material';
@@ -26,39 +27,45 @@ export default function PingCard({ ping }: Props) {
     },
   });
   const [hasPinged, setHasPinged] = useState(false);
+  const pingButton = (
+    <Button
+      disabled={hasPinged}
+      variant="contained"
+      onClick={() => {
+        setHasPinged(true);
+        pingBack();
+      }}
+      sx={{
+        fontSize: { xs: '0.8rem', sm: '1rem' },
+        flexShrink: 0,
+      }}
+    >
+      {t('pingBack')}
+    </Button>
+  );
   return (
     <Paper sx={{ p: { xs: 1, sm: 2 } }}>
-      <Stack direction="row" gap={{ xs: 1, sm: 2 }} justifyContent="space-between" alignItems="flex-start">
-        <Stack direction="row" gap={{ xs: 1, sm: 2 }}>
+      <Stack direction="row" gap={{ xs: 1, sm: 2 }} alignItems="flex-start" justifyContent="space-between">
+        <Stack direction="row" gap={{ xs: 1, sm: 2 }} alignItems="flex-start" flexGrow={1}>
           <Avatar
             src={member?.picture_path}
-            sx={{ width: { xs: 32, sm: 48 }, height: { xs: 32, sm: 48 } }}
+            sx={{ width: 48, height: 48 }}
           />
-          <Stack>
-            <Typography sx={{ fontSize: { xs: '0.8rem', sm: '1rem' } }}>
+          <Stack flexGrow={1}>
+            <Typography>
               <Link href={routes.member(member?.student_id)}>{getFullName(member)}</Link>
               {' '}
               {ping.counter > 1 ? t('hasPingedYou', { count: ping.counter }) : t('pingedYou')}
             </Typography>
-            <Typography sx={{ color: 'text.secondary', fontSize: { xs: '0.7rem', sm: '0.8rem' } }}>
-              {timeAgo(DateTime.fromISO(ping.lastPing))}
-            </Typography>
+            <Stack direction="row" gap={{ xs: 1, sm: 2 }} justifyContent="space-between">
+              <Typography sx={{ color: 'text.secondary', fontSize: '0.9rem' }}>
+                {timeAgo(DateTime.fromISO(ping.lastPing))}
+              </Typography>
+              <Box sx={{ display: { xs: 'block', sm: 'none' } }}>{pingButton}</Box>
+            </Stack>
           </Stack>
         </Stack>
-        <Button
-          disabled={hasPinged}
-          variant="contained"
-          onClick={() => {
-            setHasPinged(true);
-            pingBack();
-          }}
-          sx={{
-            fontSize: { xs: '0.7rem', sm: '1rem' },
-            flexShrink: 0,
-          }}
-        >
-          {t('pingBack')}
-        </Button>
+        <Box sx={{ display: { xs: 'none', sm: 'flex', flexShrink: 0 }, alignItems: 'flex-start' }}>{pingButton}</Box>
       </Stack>
     </Paper>
   );
