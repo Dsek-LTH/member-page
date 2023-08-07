71df7373b95ec2bfc20ef2ce4e1398895c0fe287
Daniel Adu-Gyan <danny_adu@hotmail.com>
2023-08-07 21:50:34 +0200

    :globe_with_meridians: Translate nolla guild

--- frontend/components/Nolla/locales/en.tsx
+++ frontend/components/Nolla/locales/en.tsx
@@ -2,11 +2,11 @@ export default {
   faq: {
     mentor: {
       title: 'What is a mentor?',
-      text: 'A mentor is an older student who often runs around wearing an overall, though some may run around without one. The mentors know an incredible amount of information about LTH, especially how the D-guild and student life work. A mentor knows everything that\'s on the introduction schedule, where lecture halls are located, how to order pizza, and where to go if you miss the last train home. All mentors have also undergone training in leadership, alcohol responsibility, and much, much more. So don\'t hesitate to ask them anything if a crisis arises. The mentor is there for your sake.',
+      text: "A mentor is an older student who often runs around wearing an overall, though some may run around without one. The mentors know an incredible amount of information about LTH, especially how the D-guild and student life work. A mentor knows everything that's on the introduction schedule, where lecture halls are located, how to order pizza, and where to go if you miss the last train home. All mentors have also undergone training in leadership, alcohol responsibility, and much, much more. So don't hesitate to ask them anything if a crisis arises. The mentor is there for your sake.",
     },
     pepper: {
       title: 'What is a pepper?',
-      text: 'A pepper is a person who assists the organizers of the introduction in their work of planning and carrying out the activities. They often run around wearing pink tailcoats and make sure that both the new students and mentors are excited and have a good time. The peppers also play a significant role in organizing many of the introduction’s events and pre-parties. They know everything about how the introduction works, so if you\'ve missed buying a ticket for a sittning or feel unsure about where and when to attend different events, you can always talk to a pepper!',
+      text: "A pepper is a person who assists the organizers of the introduction in their work of planning and carrying out the activities. They often run around wearing pink tailcoats and make sure that both the new students and mentors are excited and have a good time. The peppers also play a significant role in organizing many of the introduction’s events and pre-parties. They know everything about how the introduction works, so if you've missed buying a ticket for a sittning or feel unsure about where and when to attend different events, you can always talk to a pepper!",
     },
     when: {
       title: 'When does the introduction start?',
@@ -22,7 +22,188 @@ export default {
     },
     participate: {
       title: 'Do I have to participate in everything?',
-      text: 'Absolutely not! The introduction is completely voluntary, and you can participate as much as you want and feel you have time for. The introduction is there for you as a new student to find a sense of community in your new education, get to know what student life has to offer, and have a lot of fun, but if you feel that the introduction is not for you or if you simply have other things you\'d rather spend your time on, that\'s completely okay!',
+      text: "Absolutely not! The introduction is completely voluntary, and you can participate as much as you want and feel you have time for. The introduction is there for you as a new student to find a sense of community in your new education, get to know what student life has to offer, and have a lot of fun, but if you feel that the introduction is not for you or if you simply have other things you'd rather spend your time on, that's completely okay!",
+    },
+  },
+  guild: {
+    board: 'The Board',
+    chairman: {
+      title: 'Chairman',
+      text: "Hello there! My name is Sofia, and I'm a 22-year-old girl from Blekinge! They say time flies when you're having fun, and maybe that's why I still feel young despite being in my 4th year of Computer Science. As the chairman, I lead the board and the guild and represent the section externally. Last spring, I attended no less than 5 (!) balls to represent the guild. When I'm not strolling around in ball gowns, I enjoy cooking and chatting. I love talking about The Witcher (especially the books), my hometown in Blekinge (can you guess which city?), and \"today's economy,\" so don't hesitate to come up to me and say \"Hello World!\"",
+    },
+    viceChairman: {
+      title: 'Vice Chairman',
+      text: (
+        <>
+          Hey there! I&apos;m Hampus Serneke, and I&apos;m the Vice Chairman
+          here at the D-guild. I&apos;m 21 years old and currently in my second
+          year of studying Computer Science. I&apos;m a huge math nerd, but when
+          I&apos;m not indoors crunching numbers, I also love cycling and
+          running (preferably with friends). Carpe diem is my life motto
+          (I&apos;ve studied 30 credits of philosophy), so if it&apos;s
+          Wednesday, you&apos;ll find me at VGs.
+          <br />
+          As Vice Chairman, I assist Sofia and the rest of the board with
+          various tasks, but one of my responsibilities is to keep records of
+          our board and section meetings and keep our governing documents up to
+          date. I applied for this position right after the introduction was
+          over, and I highly recommend you to do the same! Don&apos;t hesitate
+          to come and talk to me if you have any questions. With that said, I
+          hope you have an amazing introduction!
+        </>
+      ),
+    },
+    activities: {
+      title: 'Head of Activities',
+      text: (
+        <>
+          Hey there! Rafael here, from D21. I&apos;m the Head of Activites
+          (AktA) and I lead the Activity Committee (AktU). AktU is a committee
+          with as many possibilities as there are members in the guild, and we
+          organize most of the social events that the section has to offer. We
+          come up with all sorts of fun activities, from our own soccer team to
+          LAN weekends to selling guild merchandise... you name it, we do it! We
+          usually have around 4 events per week, sometimes even more. You can
+          find information about what we&apos;re up to on our website (dsek.se),
+          the app, and our Instagram @aktivitetsutskottet_dsek. If there&apos;s
+          anything you&apos;d like to do, try to find me or drop a message to
+          akta@dsek.se, and we&apos;ll figure it out!
+          <br />
+          Now, who is Rafael? I grew up in the forests of Blekinge but have
+          lived in Norrtälje, Australia, and finally ended up here in Lund. I
+          love coming up with new things, and my interests include fitness,
+          plants, games, volleyball, mushrooms, hair, traveling, and musicals.
+          If we share any of these interests, I&apos;d be thrilled if you catch
+          me so we can geek out together.
+        </>
+      ),
+    },
+    information: {
+      title: 'Head of Information',
+      text: (
+        <>
+          Hello! My name is Oliver, and as you can see, I am the Head of
+          Information. That means I am responsible for the information committee
+          and I am a part of the board and am having a great time!
+          <br />
+          In the information committee, we do a lot of things. We take photos,
+          shoot videos, create graphics, write articles for the magazine, and
+          code, code, code for both the app and the website. Feel free to come
+          to me if you have any questions about the section, the information
+          committee, or coding : - ). On Spotify, you can find me as
+          &quot;olivoljan.&quot;
+        </>
+      ),
+    },
+    treasurer: {
+      title: 'Head Treasurer',
+      text: "Hello there! My name is Axel, I'm 23 years old, and I'm in my 6th year of studying computer science. As the treasurer, it's my responsibility, along with my committee, to take care of the guild’s finances and how we use them. This means we handle the guild’s bookkeeping and keep an eye on the budget. If you have any questions about money, alcohol laws, or anything else exciting, you'll probably find me in the boardroom, otherwise, you can reach me at skattm@dsek.se.",
+    },
+    facilities: {
+      title: 'Head of Facilities',
+      text: "Heyo! I'm Julia, a 23-year-old Åland Islander in my 4th year of studying Computer Science! This year, I am the Head of Facilities, which means I take care of the section's premises, inventory, and, of course, the Facilites Committee and its sub-committee, the Root Committee. The Facilities Committee is usually busy with building, painting, and crafting, doing everything needed to improve and maintain iDét, our beautiful section premises. We also handle sound and lighting, as well as taking care of the section's car, ÿalle. The Root Committee manages the guild’s servers and hardware, and we work on various fun projects, like Boss [bu:s] or our cool binary clock in iDét. If any of this sounds interesting, come and chat with me, or join us for a Fixarkväll (Fixing Evening) or Snickerboa (Crafting Workshop)! You can also reach me at kallarm@dsek.se.",
+    },
+    recreation: {
+      title: 'Head of Recreation',
+      text: "My name is Ludde, and I hold the position of Sexmästare (Head of Recreation). I'm almost 21 years old, currently in my 4th year of studying computer science (time flies!). I come from Ystad, the best city in southern Sweden. At the guild, I lead the Recreation Committee, and we are responsible for organizing all the pubs, sittningar,  and after-parties. During the introduction, you'll see us running around in our college jackets. Besides my role in the Recreation Committee, I'm also heavily involved in DWWW, where I had the responsibility of building and releasing our very own app this year! If you have any questions about the Recreation Committee or if you just want to talk code, don't hesitate to message me on Messenger or email me at sexm@dsek.se!",
+    },
+    cafe: {
+      title: 'Head of Café',
+      text: (
+        <>
+          Hello there! Do you like not starving? Do you enjoy having fun? How
+          about tying the hook to the line at the break of dawn before an
+          exciting day of cod fishing? Then, the Café Committee is the committee
+          for you! Down in iDét, we run the guild&apos;s café. There, you can
+          buy filling sandwiches, piping hot coffee, and delicious snacks. Or
+          you can just drop by for some delightful hanging out. Sometimes, we
+          organize brunches and breakfasts for when you&apos;re too hungover or
+          tired to cook yourself (wow, what a wholesome committee).
+          <br />
+          I am Arvid Carp, Head of Café, 23 years old, hailing from the
+          windswept cliffs of Kullaberg. I truly love spending time with new
+          students, so don&apos;t hesitate to approach me and have a chat if you
+          see me in iDét or at any party. Have a great time, and remember to
+          #RecycleIniDét!
+        </>
+      ),
+    },
+    corporateRelations: {
+      title: 'Head of Corporate Relations',
+      text: (
+        <>
+          Hey there! My name is Adam, and I&apos;m in my second year studying
+          InfoCom here in Lund. I come from Stockholm and enjoy the convenience
+          of having everything I could possibly need within a radius smaller
+          than the distance to my nearest subway station back home. As the Head
+          of Corporate Relations, my responsibility is to juggle all our
+          corporate partnerships and use them to bring in funds for the guild.
+          Together with these companies, we organize lunch lectures, pubs, and
+          help them get visibility in various other ways. We do everything to
+          provide you with plenty of new contacts and pave the way for your
+          dream jobs in the future.
+          <br />
+          With the funds we raise, we finance our own events, including
+          everything that happens during the introudction, the maintenance of
+          iDét, and much more. If you have any questions or fun ideas, just grab
+          me at school, send me a message on Messenger, or email me at
+          industrim@dsek.se!
+        </>
+      ),
+    },
+    studentCouncil: {
+      title: 'Head of Student Council',
+      text: (
+        <>
+          Hello there! My name is Mikolaj, and I am the head of the most
+          important committee in the section, the Student Council! Often
+          referred to as SRD, our task is to monitor and advocate for our
+          education, ensuring that your opinions about our courses are heard. We
+          have lunch meetings (almost) every Wednesday in E:1426, where we
+          discuss ongoing courses and other study-related matters. Everyone is
+          welcome to join these meetings, so feel free to drop by if you have
+          any thoughts, questions, or if you want to share your feedback about
+          your courses!
+          <br />
+          If there&apos;s anything you&apos;re wondering about, don&apos;t
+          hesitate to approach me in iDét or send an email to
+          srdordforande@dsek.se.
+        </>
+      ),
+    },
+    dchip: {
+      title: 'D-Chip',
+      text: (
+        <>
+          Welcome to the D-guild here at LTH! D-Chip is an organization working
+          to promote community and well-being among women and non-binary
+          individuals in the guild. We are thrilled to see so many new faces (or
+          &quot;Micro-chips,&quot; as we call our new members) this year!
+          <br />
+          We want you to feel welcomed and safe with us, which is why D-Chip is
+          here to provide support and create an inclusive environment where you
+          can be yourself. Throughout the year, we organize various fun events
+          such as the welcome lunch, &quot;cykelphesten&quot;, and gatherings with
+          inspiring companies. At these events, you have the opportunity to meet
+          other members and receive help during your studies. We are confident
+          that you will find many new friends with us. During the fall, we will
+          also arrange a mentoring program for you new members, where a D-Chip
+          member from an older year will be paired with you to provide support,
+          tips, and tricks for your studies! We also encourage women and
+          non-binary individuals to take an active role in the guild. By getting
+          involved with D-Chip, you can contribute to making a difference and
+          promoting equality and inclusion within the guild. Don&apos;t hesitate to
+          reach out to us if you have any questions or need support. We are here
+          for you! You can talk to the members of the board or follow us on
+          social media @dchip to stay updated on all the exciting things we are
+          planning together. We are looking forward to meeting you at our
+          upcoming events. Until then, take care of yourself and each other!
+          <div>Warm welcome to the D-guild and D-Chip!</div>
+          <br />
+          <div>Best regards,</div>
+          <i>The Board of D-Chip</i>
+        </>
+      ),
     },
   },
 };
