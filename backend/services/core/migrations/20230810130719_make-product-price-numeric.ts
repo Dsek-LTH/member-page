a14b895bd46a51055638feda1a6e6f1936753296
Oliver Levay <oliver.levay@hotmail.com>
2023-08-22 13:48:40 +0200

    :sparkles: Make more admin for the webshop

--- backend/services/core/migrations/20230810130719_make-product-price-numeric.ts
+++ backend/services/core/migrations/20230810130719_make-product-price-numeric.ts
@@ -1,5 +1,4 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('product', (table) => {
@@ -7,10 +6,8 @@ export async function up(knex: Knex): Promise<void> {
   });
 }
 
-
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable('product', (table) => {
     table.integer('price').notNullable().alter();
   });
 }
-
