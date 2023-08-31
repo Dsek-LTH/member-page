9e10e278c04b6c40f894acf8b1c280893894794d
Ludvig <ludvig.svedberg@mac.com>
2023-08-31 15:22:30 +0200

    Fix and comment migration

--- backend/services/core/migrations/20230822094308_add_custom_author_type_to_articles.ts
+++ backend/services/core/migrations/20230822094308_add_custom_author_type_to_articles.ts
@@ -4,16 +4,16 @@ export async function up(knex: Knex): Promise<void> {
   // Add custom authors table
   await knex.schema.createTable('custom_authors', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
-    table.string('name').notNullable();
-    table.string('name_en');
-    table.string('image_url');
+    table.string('name').notNullable().comment('Swedish name of custom author');
+    table.string('name_en').comment('English name of custom author, possibly null');
+    table.string('image_url').comment('Image url of custom author, possibly null');
     table.timestamps(true, true);
   });
   await knex.schema.createTable('custom_author_roles', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
     table.uuid('custom_author_id').unsigned().notNullable().references('custom_authors.id')
       .onDelete('CASCADE');
-    table.string('role').unsigned().notNullable();
+    table.string('role').unsigned().notNullable().comment('should use role syntax, such as dsek.styr or dsek.stab.noll');
     table.timestamps(true, true);
   });
   // Add new "authors" table
@@ -29,20 +29,25 @@ export async function up(knex: Knex): Promise<void> {
     table.uuid('temp_article_id').unsigned().notNullable().references('articles.id')
       .comment('Temporary column during migration');
   });
-  const memberAuthorArticles = await knex('articles').select('id', 'author_id', 'author_type')
+  // Get all articles posted by a member, not as a mandate
+  const memberAuthorArticles = await knex('articles').select('id', 'author_id', 'author_type', 'published_datetime', 'latest_edit_datetime')
     .where({ author_type: 'Member' });
-  const mandateAuthorArticles = await knex('articles').select('articles.id', 'author_id', 'author_type', 'mandates.member_id', 'mandates.id as mandate_id')
+  // Get all articles posted as a mandate
+  const mandateAuthorArticles = await knex('articles').select('articles.id', 'author_id', 'author_type', 'mandates.member_id', 'mandates.id as mandate_id', 'published_datetime', 'latest_edit_datetime')
     .join('mandates', 'mandates.id', '=', 'articles.author_id')
     .where({ author_type: 'Mandate' });
-  // Move all author data from articles to the new table
+  // Create rows in authors table for each article posted as a member
   if (memberAuthorArticles.length > 0) {
     await knex('authors')
       .insert(memberAuthorArticles.map((article) => ({
         member_id: article.author_id,
         temp_article_id: article.id,
         type: 'Member',
+        created_at: article.published_datetime,
+        updated_at: article.latest_edit_datetime,
       })));
   }
+  // Create rows in authors table for each article posted as a mandate
   if (mandateAuthorArticles.length > 0) {
     await knex('authors')
       .insert(mandateAuthorArticles.map((article) => ({
@@ -50,8 +55,12 @@ export async function up(knex: Knex): Promise<void> {
         mandate_id: article.mandate_id,
         temp_article_id: article.id,
         type: 'Mandate',
+        created_at: article.published_datetime,
+        updated_at: article.latest_edit_datetime,
       })));
   }
+  // Update all articles to point to the newly created author rows,
+  // using the temporary article_id column
   await knex.raw(`
     UPDATE articles
     SET author_id = authors.id
@@ -76,17 +85,21 @@ export async function down(knex: Knex): Promise<void> {
     table.dropForeign('author_id');
     table.string('author_type').notNullable().defaultTo('Member').comment('What type the author is (e.g. Member and Mandate)');
   });
-  await knex
-    .update({ author_id: knex.raw('authors.member_id'), author_type: 'Member' })
-    .from('articles')
-    .join('authors', 'authors.id', '=', 'articles.author_id')
-    .where({ 'authors.type': 'Member' });
-  await knex
-    .update({ author_id: knex.raw('authors.mandate_id'), author_type: 'Mandate' })
-    .from('articles')
-    .join('authors', 'authors.id', '=', 'articles.author_id')
-    .where({ 'authors.type': 'Mandate' });
+  await knex.raw(`
+    UPDATE articles
+    SET author_id = authors.member_id
+    FROM authors
+    WHERE authors.id = articles.author_id
+    AND authors.type = 'Member'
+  `);
+  await knex.raw(`
+    UPDATE articles
+    SET author_id = authors.mandate_id
+    FROM authors
+    WHERE authors.id = articles.author_id
+    AND authors.type = 'Mandate'
+  `);
   await knex.schema.dropTable('authors');
-  await knex.schema.dropTable('custom_author_positions');
+  await knex.schema.dropTable('custom_author_roles');
   await knex.schema.dropTable('custom_authors');
 }
