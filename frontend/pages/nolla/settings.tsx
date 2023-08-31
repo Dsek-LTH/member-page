3256cf9983655071355a74720f6fbcbb7b2c9042
Ludvig <ludvig.svedberg@mac.com>
2023-08-31 19:18:16 +0200

    Add paper background to nolla settings page

--- frontend/pages/nolla/settings.tsx
+++ frontend/pages/nolla/settings.tsx
@@ -1,18 +1,21 @@
-import { Stack } from '@mui/material';
+import { Paper } from '@mui/material';
+import DarkModeSelector from '~/components/Header/components/DarkModeSelector';
+import LanguageSelector from '~/components/Header/components/LanguageSelector';
 import NollaLayout from '~/components/Nolla/layout';
 import theme from '~/components/Nolla/theme';
 import genGetProps from '~/functions/genGetServerSideProps';
 import SettingsPage from '../settings';
-import LanguageSelector from '~/components/Header/components/LanguageSelector';
-import DarkModeSelector from '~/components/Header/components/DarkModeSelector';
 
 function Settings() {
   return (
     <>
-      <Stack spacing={1} direction="row">
+      <Paper sx={{
+        p: 2, alignSelf: 'flex-start', display: 'inline-flex', flexDirection: 'row', gap: 1,
+      }}
+      >
         <LanguageSelector />
         <DarkModeSelector />
-      </Stack>
+      </Paper>
       <SettingsPage />
     </>
   );
