a268c46b93b08309fa58fb7de80a0ae48fe3ab1a
Oliver Levay <oliver.levay@hotmail.com>
2023-09-05 16:47:42 +0200

    :sparkles: Give Boss Sentience

--- frontend/pages/api/boss.ts
+++ frontend/pages/api/boss.ts
@@ -3,9 +3,14 @@ import {
   ApolloClient, createHttpLink, InMemoryCache,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import OpenAI from 'openai';
 import { MeHeaderDocument, MeHeaderQuery } from '~/generated/graphql';
 import blockedIDs from '~/data/blockedIDs';
 import { MAX_MESSAGE_LENGTH } from '~/data/boss';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
@@ -15,32 +20,39 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const {
     message, red, green, blue, authToken,
   } = JSON.parse(req.body);
-  const httpLink = createHttpLink({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_ADDRESS,
-  });
+  let id; let
+    name;
+  if (process.env.NODE_ENV === 'development') {
+    id = 'ol1662le-s';
+    name = 'Oliver';
+  } else {
+    const httpLink = createHttpLink({
+      uri: process.env.NEXT_PUBLIC_GRAPHQL_ADDRESS,
+    });
 
-  const authLink = setContext((_, { headers }) => ({
-    headers: {
-      ...headers,
-      authorization: authToken ? `Bearer ${authToken}` : '',
-    },
-  }));
-
-  const client = new ApolloClient({
-    ssrMode: true,
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-    defaultOptions: {
-      query: {
-        errorPolicy: 'all',
+    const authLink = setContext((_, { headers }) => ({
+      headers: {
+        ...headers,
+        authorization: authToken ? `Bearer ${authToken}` : '',
       },
-    },
-  });
+    }));
 
-  const { data } = await client.query<MeHeaderQuery>({ query: MeHeaderDocument });
+    const client = new ApolloClient({
+      ssrMode: true,
+      link: authLink.concat(httpLink),
+      cache: new InMemoryCache(),
+      defaultOptions: {
+        query: {
+          errorPolicy: 'all',
+        },
+      },
+    });
 
-  const id = data?.me?.student_id;
-  const name = data?.me?.first_name;
+    const { data } = await client.query<MeHeaderQuery>({ query: MeHeaderDocument });
+
+    id = data?.me?.student_id;
+    name = data?.me?.first_name;
+  }
   if (!id || !name) {
     return res.status(500).json({ success: false, message: 'Student id not found (try reloading the page)' });
   }
@@ -53,12 +65,45 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(500).json({ success: false, message: 'Message too long!' });
   }
 
+  const question = `${message} //${name}`;
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'user',
+        content: `
+    Efter denna förklaring kommer du att få en fråga. Vi ber dig att svara på frågan väldigt kortfattat oavsett vad frågan är.
+    Du är skapad av en grupp studenter på LTH som heter D-sektionen.
+    Du är en LED skärm som sitter i iDét, källarlokalen i E-huset på Lunds tekniska högskola (LTH) som tillhör D-sektionen.
+    Du heter Boss men det uttalas Bus.
+    D-sektionen är en sektion där studenter som studerar Datateknik och Information- och kommunikationssteknik på LTH är medlemmar.
+    Du har en RGB LED skärm som kan visa olika färger. Vi väljer alltid färg åt dig, men du kan få veta vilken färg som används.
+    Våra medlemmar kommer ställa frågor till dig, och det vore fint om du kunde svara som om du vore en robot med artificiell intelligens, känslor tankar och en personlighet.
+    Du gillar 1337, gaming och att koda långt in i småtimmarna. Du har starka åsikter om programmering och tycker att alla borde lära sig programmera.
+    Du har åsikter i olika frågor, och om man frågar om en åsikt så kommer du svara med en åsikt.
+    Svara på frågan med en JSON som ser ut på detta vis.
+    {
+      answer: "Detta är ett svar"
+    }
+    Färgen du har fått just nu är: rgb(${red}, ${green}, ${blue})
+    Här kommer frågan: ${question}
+    `,
+      },
+    ],
+    model: 'gpt-3.5-turbo',
+  });
+  const { answer } = JSON.parse(chatCompletion.choices[0].message.content);
+
   blockedIDs.add(id);
   setTimeout(() => blockedIDs.delete(id), process.env.SANDBOX === 'true' ? 5000 : 1000 * 60);
-
-  const sentMessage = `${message} //${name}`;
   // eslint-disable-next-line no-console
-  console.log(`boss: ${id} sent message ${sentMessage}`);
-  const response = await fetch(`http://192.168.7.170:8080/sendText?message=${sentMessage}&color=${red},${green},${blue}`, { method: 'POST' });
-  return res.status(200).json({ success: response.ok, message: sentMessage });
+  console.log(`boss: ${id} sent message ${question}`);
+  let response = { ok: true };
+  if (process.env.NODE_ENV !== 'development') {
+    response = await fetch(`http://192.168.7.170:8080/sendText?message=Q: ${question}, A: ${answer}&color=${red},${green},${blue}`, { method: 'POST' });
+  }
+  return res.status(200).json({
+    success: response.ok,
+    question,
+    answer,
+  });
 }
--- frontend/pages/mojt/boss.tsx
+++ frontend/pages/mojt/boss.tsx
@@ -1,6 +1,11 @@
-import
-{
-  Alert, Button, Stack, TextField, Typography,
+import {
+  Alert,
+  Button,
+  CircularProgress,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
 } from '@mui/material';
 import { useSession } from 'next-auth/react';
 import { useState } from 'react';
@@ -9,6 +14,7 @@ import { useUser } from '~/providers/UserProvider';
 import { MAX_MESSAGE_LENGTH } from '../../data/boss';
 import { useSetPageName } from '~/providers/PageNameProvider';
 import PageHeader from '~/components/PageHeader';
+import Message, { MessageType } from '~/components/Mojt/Message';
 
 export default function BossPage() {
   useSetPageName('boss');
@@ -16,10 +22,58 @@ export default function BossPage() {
   const { user } = useUser();
   const [status, setStatus] = useState('');
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState('');
   const [red, setRed] = useState('255');
   const [green, setGreen] = useState('255');
   const [blue, setBlue] = useState('255');
+  const [messages, setMessages] = useState<MessageType[]>([]);
+
+  async function sendMessage() {
+    const data = {
+      message,
+      red,
+      green,
+      blue,
+      authToken: session.accessToken,
+    };
+    setStatus('');
+    setError(false);
+    setLoading(true);
+    setMessages((prev) => {
+      const newMessages = [...prev];
+      newMessages.push({
+        content: `${message} //${user.first_name}`,
+        sentByYou: true,
+        color: `rgb(${red}, ${green}, ${blue})`,
+      });
+      return newMessages;
+    });
+    fetch('/api/boss', { method: 'POST', body: JSON.stringify(data) })
+      .then((res) => res.json())
+      .then((resData) => {
+        if (resData.success) {
+          setStatus(`Sent message: ${resData.message}`);
+          setMessages((prev) => {
+            const newMessages = [...prev];
+            newMessages.push({
+              content: resData.answer,
+              sentByYou: false,
+              color: `rgb(${red}, ${green}, ${blue})`,
+            });
+            return newMessages;
+          });
+        } else {
+          setStatus(`Error: ${resData.message}`);
+          setError(true);
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+    setMessage('');
+  }
+
   if (!user?.first_name) {
     return (
       <>
@@ -32,84 +86,109 @@ export default function BossPage() {
   return (
     <>
       <PageHeader>boss</PageHeader>
-      <Stack maxWidth={500} spacing={1}>
+      <Stack
+        maxWidth={500}
+        spacing={1}
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+            e.preventDefault();
+            sendMessage();
+          }
+        }}
+      >
         <Typography>
-          Upprepade olämpliga meddelanden kan leda till avstängining.
-          Ditt förnamn kommer att skrivas ut efter ditt meddelande.
+          Upprepade olämpliga meddelanden kan leda till avstängining. Ditt
+          förnamn kommer att skrivas ut efter ditt meddelande.
         </Typography>
-        <TextField
-          id="message"
-          label="Message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          inputProps={{
-            maxLength: MAX_MESSAGE_LENGTH,
-          }}
-        />
+        <Stack direction="row" spacing={1}>
+          <TextField
+            id="red"
+            label="Red"
+            value={red}
+            type="number"
+            InputProps={{
+              inputProps: { max: 255, min: 0 },
+              style: { color: 'red' },
+            }}
+            onChange={(e) => setRed(e.target.value)}
+          />
 
-        <TextField
-          id="red"
-          label="Red"
-          value={red}
-          type="number"
-          InputProps={{
-            inputProps: { max: 255, min: 0 },
-            style: { color: 'red' },
-          }}
-          onChange={(e) => setRed(e.target.value)}
-        />
+          <TextField
+            id="green"
+            label="Green"
+            value={green}
+            type="number"
+            InputProps={{
+              inputProps: { max: 255, min: 0 },
+              style: { color: 'green' },
+            }}
+            onChange={(e) => setGreen(e.target.value)}
+          />
+          <TextField
+            id="blue"
+            label="Blue"
+            value={blue}
+            type="number"
+            InputProps={{
+              inputProps: { max: 255, min: 0 },
+              style: { color: 'blue' },
+            }}
+            onChange={(e) => setBlue(e.target.value)}
+          />
+        </Stack>
+        <Paper>
+          <Stack padding={2} minHeight={600} spacing={2}>
+            <Stack spacing={2} paddingBottom={3}>
+              {messages.map((msg) => (
+                <Message message={msg} key={msg.content} />
+              ))}
+              {loading && (
+                <CircularProgress
+                  sx={{
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                  }}
+                />
+              )}
+            </Stack>
+            <TextField
+              style={{
+                marginTop: 'auto',
+              }}
+              id="message"
+              label="Message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              inputProps={{
+                maxLength: MAX_MESSAGE_LENGTH,
+              }}
+            />
+          </Stack>
+        </Paper>
 
-        <TextField
-          id="green"
-          label="Green"
-          value={green}
-          type="number"
-          InputProps={{
-            inputProps: { max: 255, min: 0 },
-            style: { color: 'green' },
-          }}
-          onChange={(e) => setGreen(e.target.value)}
-        />
-
-        <TextField
-          id="blue"
-          label="Blue"
-          value={blue}
-          type="number"
-          InputProps={{
-            inputProps: { max: 255, min: 0 },
-            style: { color: 'blue' },
-          }}
-          onChange={(e) => setBlue(e.target.value)}
-        />
-        <Typography>Message preview:</Typography>
-        <Typography sx={{ color: `rgb(${red}, ${green}, ${blue})`, backgroundColor: 'black', padding: '0.5rem' }}>
-          {`${message} //${user.first_name}`}
-        </Typography>
         <Button
           variant="contained"
           disabled={!message || !red || !green || !blue}
           onClick={() => {
-            const data = {
-              message, red, green, blue, authToken: session.accessToken,
-            };
-            setStatus('');
-            setError(false);
-            fetch('/api/boss', { method: 'POST', body: JSON.stringify(data) })
-              .then((res) => res.json())
-              .then((resData) => {
-                if (resData.success) {
-                  setStatus(`Sent message: ${resData.message}`);
-                } else {
-                  setStatus(`Error: ${resData.message}`);
-                  setError(true);
-                }
-              });
+            sendMessage();
           }}
         >
           Skicka
         </Button>
-        <Alert severity={error ? 'error' : 'success'} sx={{ display: status ? 'flex' : 'none' }}>
+        <Typography>Message preview:</Typography>
+        <Typography
+          sx={{
+            color: `rgb(${red}, ${green}, ${blue})`,
+            backgroundColor: 'black',
+            padding: '0.5rem',
+          }}
+        >
+          {`${message} //${user.first_name}`}
+        </Typography>
+        <Alert
+          severity={error ? 'error' : 'success'}
+          sx={{ display: status ? 'flex' : 'none' }}
+        >
           {status}
         </Alert>
       </Stack>
